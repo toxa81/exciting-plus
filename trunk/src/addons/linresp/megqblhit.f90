c255f63bd2f4f7346d53ad21d5176168c46c5298
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2009-11-10 11:58:38 +0000

    optimized ME

--- trunk/src/addons/linresp/megqblhit.f90
+++ trunk/src/addons/linresp/megqblhit.f90
@@ -18,7 +18,8 @@ integer, intent(in) :: ik
 integer, intent(in) :: jk
 
 complex(8), allocatable :: mit(:,:)
-complex(8), allocatable :: a(:,:,:) 
+!complex(8), allocatable :: a(:,:,:) 
+complex(8), allocatable :: a(:) 
 complex(8), allocatable :: megq_tmp(:,:)
 
 integer ig,ist1,ist2,i,ispn,ispn2,j,ist
@@ -26,50 +27,83 @@ integer idx0,bs,idx_g1,idx_g2,igp,ifg,ir,i1,i2
 complex(8) zt1
 logical l1
 complex(8), external :: zdotu,zdotc
+integer ist1_prev
 
 allocate(megq_tmp(ngvecme,nmegqblhmax))
 megq_tmp=zzero
 
-allocate(a(ngknr1,nstsv,nspinor))
+!allocate(a(ngknr1,nstsv,nspinor))
+allocate(a(ngknr2))
 allocate(mit(ngknr1,ngknr2))
 
+! split G-vectors between processors
 call idxbos(ngvecme,mpi_dims(2),mpi_x(2)+1,idx0,bs)
 idx_g1=idx0+1
 idx_g2=idx0+bs
 
+ist1_prev=-1
 do ig=idx_g1,idx_g2
   call genpwit2(ngknr1,ngknr2,igkignr1,igkignr2,-(ivg(:,ig+gvecme1-1)+ivg(:,igkq)),gvit,mit)
-  a=zzero
-  do ispn=1,nspinor
-    do i=1,nstsv
-      l1=.true.
-      if (spinpol) then
-        if (spinor_ud(ispn,i,ik).eq.0) l1=.false.
-      endif
-      if (l1) then
-        call zgemv('N',ngknr1,ngknr2,zone,mit,ngknr1,wfsvit2(1,ispn,i),1,zzero,a(1,i,ispn),1)
-      endif
-    enddo
-  enddo
   do ispn=1,nspinor
     ispn2=ispn
-    if (lrtype.eq.1) then
-      ispn2=3-ispn
-    endif
+    if (lrtype.eq.1) ispn2=3-ispn
     do i=1,nmegqblh_
       ist1=bmegqblh_(1,i)
       ist2=bmegqblh_(2,i)
+! precompute
+      if (ist1.ne.ist1_prev) then
+        l1=.true.
+        if (spinpol) then
+          if (spinor_ud(ispn,ist1,ik).eq.0) l1=.false.
+        endif
+        if (l1) call zgemv('T',ngknr1,ngknr2,zone,mit,ngknr1,&
+          dconjg(wfsvit1(:,ispn,ist1)),1,zzero,a,1)
+        ist1_prev=ist1
+      endif
       l1=.true.
       if (spinpol) then
-        if (spinor_ud(ispn,ist1,ik).eq.0.or.spinor_ud(ispn2,ist2,jk).eq.0) l1=.false.
+        if (spinor_ud(ispn2,ist2,jk).eq.0) l1=.false.
       endif
-      if (l1) then
-        megq_tmp(ig,i)=megq_tmp(ig,i)+&
-          zdotc(ngknr1,wfsvit1(1,ispn,ist1),1,a(1,ist2,ispn2),1)
-      endif
-    enddo
-  enddo
-enddo !ig  
+      if (l1) megq_tmp(ig,i)=megq_tmp(ig,i)+&
+          zdotu(ngknr2,wfsvit2(1,ispn2,ist2),1,a,1)
+    enddo !i  
+  enddo !ispn
+enddo !ig
+
+  
+!  
+!  a=zzero
+!  do ispn=1,nspinor
+!    do i=1,nstsv
+!      l1=.true.
+!      if (spinpol) then
+!        if (spinor_ud(ispn,i,ik).eq.0) l1=.false.
+!      endif
+!      if (l1) then
+!        call zgemv('N',ngknr1,ngknr2,zone,mit,ngknr1,wfsvit2(1,ispn,i),1,zzero,a(1,i,ispn),1)
+!      endif
+!    enddo
+!  enddo
+!  do ispn=1,nspinor
+!    ispn2=ispn
+!    if (lrtype.eq.1) then
+!      ispn2=3-ispn
+!    endif
+!    do i=1,nmegqblh_
+!      ist1=bmegqblh_(1,i)
+!      ist2=bmegqblh_(2,i)
+!      l1=.true.
+!      if (spinpol) then
+!        if (spinor_ud(ispn,ist1,ik).eq.0.or.spinor_ud(ispn2,ist2,jk).eq.0) l1=.false.
+!      endif
+!      if (l1) then
+!        megq_tmp(ig,i)=megq_tmp(ig,i)+&
+!          zdotc(ngknr1,wfsvit1(1,ispn,ist1),1,a(1,ist2,ispn2),1)
+!      endif
+!    enddo
+!  enddo
+!enddo !ig  
+
 deallocate(mit,a)
 
 if (mpi_dims(2).gt.1) then
--- trunk/src/addons/linresp/megqblhmt.f90
+++ trunk/src/addons/linresp/megqblhmt.f90
@@ -17,9 +17,12 @@ integer, intent(in) :: jk
 ! local variables
 integer ig,i,j,ist1,ist2,ias,io1,io2,lm1,lm2,ispn,ispn2
 integer idx_g1,idx_g2,idx0,bs
-complex(8) a1(lmmaxvr,nrfmax),a2(lmmaxvr,nrfmax)
+complex(8) a1(lmmaxvr,nrfmax),a2(lmmaxvr,nrfmax),a1t(lmmaxvr,nrfmax,natmtot)
 complex(8), allocatable :: megq_tmp(:,:)
 logical l1
+integer ist1_prev
+complex(8), external :: zdotu,zdotc
+
 
 allocate(megq_tmp(ngvecme,nmegqblhmax))
 megq_tmp=zzero
@@ -28,34 +31,52 @@ call idxbos(ngvecme,mpi_dims(2),mpi_x(2)+1,idx0,bs)
 idx_g1=idx0+1
 idx_g2=idx0+bs
 
+ist1_prev=-1
 do ig=idx_g1,idx_g2
   do ispn=1,nspinor
     ispn2=ispn
-    if (lrtype.eq.1) then
-      ispn2=3-ispn
-    endif
+    if (lrtype.eq.1) ispn2=3-ispn
     do i=1,nmegqblh_
       ist1=bmegqblh_(1,i)
       ist2=bmegqblh_(2,i)
+! precompute
+      if (ist1.ne.ist1_prev) then
+        a1t=zzero
+        l1=.true.
+        if (spinpol) then
+          if (spinor_ud(ispn,ist1,ik).eq.0) l1=.false.
+        endif
+        if (l1) then
+		  do ias=1,natmtot
+			a1=dconjg(wfsvmt1(:,:,ias,ispn,ist1))
+			do j=1,ngu(ias,ig)
+			  lm1=igu(1,j,ias,ig)
+			  lm2=igu(2,j,ias,ig)
+			  io1=igu(3,j,ias,ig)
+			  io2=igu(4,j,ias,ig)
+			  a1t(lm2,io2,ias)=a1t(lm2,io2,ias)+&
+			    dconjg(wfsvmt1(lm1,io1,ias,ispn,ist1))*gu(j,ias,ig)
+			enddo !j
+		  enddo !ias
+		  ist1_prev=ist1
+		endif
+      endif
       l1=.true.
       if (spinpol) then
-        if (spinor_ud(ispn,ist1,ik).eq.0.or.spinor_ud(ispn2,ist2,jk).eq.0) l1=.false.
+        if (spinor_ud(ispn2,ist2,jk).eq.0) l1=.false.
       endif
       if (l1) then
         do ias=1,natmtot
-          a1=dconjg(wfsvmt1(:,:,ias,ispn,ist1))
-          a2=wfsvmt2(:,:,ias,ispn2,ist2)
-          do j=1,ngu(ias,ig)
-            lm1=igu(1,j,ias,ig)
-            lm2=igu(2,j,ias,ig)
-            io1=igu(3,j,ias,ig)
-            io2=igu(4,j,ias,ig)
-            megq_tmp(ig,i)=megq_tmp(ig,i)+a1(lm1,io1)*a2(lm2,io2)*gu(j,ias,ig)
-          enddo  
+          megq_tmp(ig,i)=megq_tmp(ig,i)+zdotu(lmmaxvr,a1t(1,1,ias),1,&
+            wfsvmt2(1,1,ias,ispn2,ist2),1)
+          do io2=2,nrfmax
+            megq_tmp(ig,i)=megq_tmp(ig,i)+zdotu(16,a1t(1,io2,ias),1,&
+              wfsvmt2(1,io2,ias,ispn2,ist2),1)    
+          enddo
         enddo !ias
       endif
     enddo !i
-  enddo
+  enddo !ispn
 enddo !ig    
 
 if (mpi_dims(2).gt.1) then
