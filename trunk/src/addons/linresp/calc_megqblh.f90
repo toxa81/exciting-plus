fcc98edddb356409460856888380204c9240a7e8
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2009-11-24 12:35:30 +0000

    experimental: FFT in interstitial for ME

--- trunk/src/addons/linresp/calc_megqblh.f90
+++ trunk/src/addons/linresp/calc_megqblh.f90
@@ -32,6 +32,11 @@ logical l1
 complex(8), allocatable :: mit(:,:)
 complex(8), allocatable :: a1it(:) 
 complex(8) a1mt(lmmaxvr,nrfmax,natmtot)
+complex(8), allocatable :: wfir1(:)
+complex(8), allocatable :: wfir2(:)
+integer ivg1(3),ig1,ig2,ifg,ir
+complex(8) zt1
+
 ! external
 complex(8), external :: zdotu
 
@@ -45,6 +50,8 @@ idx_g2=idx0+bs
 
 allocate(a1it(ngknr2))
 allocate(mit(ngknr1,ngknr2))
+allocate(wfir1(ngrtot))
+allocate(wfir2(ngrtot))
 
 do ig=idx_g1,idx_g2
   call timer_start(5)
@@ -105,19 +112,107 @@ do ig=idx_g1,idx_g2
         call timer_stop(4)
 ! interstitial part
         call timer_start(5) 
-        megqblh_(ig,i)=megqblh_(ig,i)+zdotu(ngknr2,wfsvit2(1,ispn2,ist2),1, &
-          a1it,1)
+!        megqblh_(ig,i)=megqblh_(ig,i)+zdotu(ngknr2,wfsvit2(1,ispn2,ist2),1, &
+!          a1it,1)
         call timer_stop(5)
       endif
     enddo !i
   enddo !ispn
 enddo !ig    
 
+ist1_prev=-1
+do ispn=1,nspinor
+  ispn2=ispn
+  if (lrtype.eq.1) ispn2=3-ispn
+  do i=1,nmegqblh_
+	ist1=bmegqblh_(1,i)
+	ist2=bmegqblh_(2,i)
+! precompute
+	if (ist1.ne.ist1_prev) then
+	  wfir1=zzero
+	  l1=.true.
+	  if (spinpol) then
+		if (spinor_ud(ispn,ist1,ik).eq.0) l1=.false.
+	  endif
+	  if (l1) then
+		call timer_start(5)
+! interstitial part
+		do ig1=1,ngknr1
+		  ifg=igfft(igkignr1(ig1))
+		  wfir1(ifg)=wfsvit1(ig1,ispn,ist1)
+		enddo
+		call zfftifc(3,ngrid,1,wfir1)
+		do ir=1,ngrtot
+		  wfir1(ir)=wfir1(ir)*cfunir(ir)
+		enddo
+        call zfftifc(3,ngrid,-1,wfir1)
+		call timer_stop(5)
+	  endif
+	  ist1_prev=ist1
+	endif !ist1.ne.ist1_prev
+	l1=.true.
+	if (spinpol) then
+	  if (spinor_ud(ispn2,ist2,jk).eq.0) l1=.false.
+	endif
+	if (l1) then
+! interstitial part
+	  call timer_start(5) 
+      do ig=1,ngvecme
+        zt1=zzero
+        do ig2=1,ngknr2
+          ivg1(:)=ivg(:,igkignr2(ig2))-ivg(:,ig)-ivg(:,igkq)
+          ifg=igfft(ivgig(ivg1(1),ivg1(2),ivg1(3)))
+          zt1=zt1+dconjg(wfir1(ifg))*wfsvit2(ig2,ispn2,ist2)
+	    enddo !ig2
+	    megqblh_(ig,i)=megqblh_(ig,i)+zt1
+	  enddo !ig
+	  call timer_stop(5)
+	endif
+  enddo !i
+enddo !ispn
+
+
+
+
+
+
+
 if (mpi_dims(2).gt.1) then
   call d_reduce_cart(comm_cart_010,.false.,megqblh_,2*ngvecme*nmegqblhmax)
 endif
+
+!! interstitial part
+!do ir=1,ngrtot
+!  zrhoir(ir)=zrhoir(ir)*cfunir(ir)*omega
+!enddo
+!call zfftifc(3,ngrid,-1,zrhoir)
+!do ig=1,ngvec_me
+!  zrhofc(ig,2)=zrhoir(igfft1(ig))
+!  zrhofc(ig,3)=zrhofc(ig,1)+zrhofc(ig,2)
+!enddo
 
 deallocate(a1it,mit)
 
 return
 end
+
+!subroutine calcwfir(ngp,wfsvit,wfir)
+!integer, intent(in) :: ngp
+!complex(8), intent(in) :: wfsvit(ngkmax)
+!complex(8), intent(out) :: wfir(ngrtot)
+!use modmain
+!implicit none
+!real(8) t1
+!integer igp,ifg
+!
+!wfir=zzero
+!t1=1.d0/sqrt(omega)
+!
+!do igp=1,ngp
+!  ifg=igfft(igpig(igp))
+!  wfir(ifg)=t1*wfsvit(igp)
+!enddo
+!call zfftifc(3,ngrid,1,wfir)
+!
+!return
+!end
