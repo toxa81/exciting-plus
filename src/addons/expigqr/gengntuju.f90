eb036921bca21ccf958a928dc198280eebe42a9e
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2010-09-30 11:00:57 +0000

    minior optimization in matrix elements

--- src/addons/expigqr/gengntuju.f90
+++ src/addons/expigqr/gengntuju.f90
@@ -11,12 +11,15 @@ integer ig,is,ir,n,ias,io1,io2,l1,m1,lm1,l2,m2,lm2,l3,m3,lm3,ic
 integer igloc,ngvecmeloc
 integer lmmaxexp
 complex(8) zt1
-real(8) fr(nrmtmax) !,gr(nrmtmax),cf(4,nrmtmax)
+real(8) fr(nrmtmax)
 real(8), allocatable :: jl(:,:)
 real(8), allocatable :: uju(:,:,:,:,:)
 real(8), allocatable :: gnt(:,:,:)
 real(8), external :: gaunt
 real(8), external :: rfinteg
+complex(8), allocatable :: zm(:,:,:)
+integer, parameter :: ngvb=16
+integer i
 
 lmmaxexp=(lmaxexp+1)**2
 allocate(gnt(lmmaxexp,lmmaxvr,lmmaxvr))
@@ -36,10 +39,9 @@ do l1=0,lmaxvr
     enddo
   enddo
 enddo
-
 allocate(jl(nrmtmax,0:lmaxexp))
 allocate(uju(0:lmaxexp,0:lmaxvr,0:lmaxvr,nufrmax,nufrmax))
-
+allocate(zm(0:lmaxexp,lmmaxvr,lmmaxvr))
 igntuju=0
 ngntuju=0
 gntuju=zzero
@@ -47,6 +49,25 @@ gntuju=zzero
 ngvecmeloc=mpi_grid_map(ngvecme,dim_k)
 do igloc=1,ngvecmeloc
   ig=mpi_grid_map(ngvecme,dim_k,loc=igloc)
+! precompute atom-independent array
+  do l1=0,lmaxvr
+    do m1=-l1,l1 
+      lm1=idxlm(l1,m1)
+      do l2=0,lmaxvr
+        do m2=-l2,l2
+          lm2=idxlm(l2,m2)
+          do l3=0,lmaxexp
+            zt1=zzero
+            do m3=-l3,l3
+              lm3=idxlm(l3,m3)
+              zt1=zt1+gnt(lm3,lm2,lm1)*ylmgq(lm3,ig)*dconjg(zi**l3)
+            enddo !m3
+            zm(l3,lm2,lm1)=zt1*fourpi
+          enddo !l3
+        enddo
+      enddo
+    enddo
+  enddo
 ! loop over atom classes
   do ic=1,natmcls
     ias=ic2ias(ic)
@@ -59,8 +80,8 @@ do igloc=1,ngvecmeloc
     do l3=0,lmaxexp
       do l1=0,lmaxvr
         do l2=0,lmaxvr
-          do io1=1,nufrmax
-            do io2=1,nufrmax
+          do io1=1,nufr(l1,is)
+            do io2=1,nufr(l2,is)
               do ir=1,nrmt(is)
                 fr(ir)=ufr(ir,l1,io1,ic)*ufr(ir,l2,io2,ic)*jl(ir,l3)*&
                        (spr(ir,is)**2)
@@ -74,7 +95,7 @@ do igloc=1,ngvecmeloc
       enddo !l1
     enddo !l3
 ! compute muffin-tin integrals
-!  1) sfacgq0 and ylmgq0 are generated for exp^{+i(G+q)x}
+!  1) sfacgq and ylmgq are generated for exp^{+i(G+q)x}
 !     expansion of a plane-wave: 
 !       exp^{+igx}=4\pi \sum_{l_3 m_3} i^{l_3} j_{l_3}(gr)Y_{l_3 m_3}^{*}(\hat g)Y_{l_3 m_3}(\hat r)
 !     but we need exp^{-i(G+q)x}, so expansion terms will be conjugated
@@ -83,7 +104,9 @@ do igloc=1,ngvecmeloc
 !       = \int d \Omega Y_{l_1 m_1}^{*}Y_{l_3 m_3}^{*} Y_{l_2 m_2} = gaunt coeff, which is real
 !     so we can conjugate the integral:
 !     \int d \Omega Y_{l_1 m_1} Y_{l_3 m_3} Y_{l_2 m_2}^{*} = gaunt(lm2,lm1,lm3)
+!  2*) gnt array has different order of indices: gnt(lm3,lm2,lm1)=gaunt(lm2,lm1,lm3)
 !  3) we can sum over lm3 index of a plane-wave expansion
+!  3*) we can sum over m3 index and get atom-idependent array
 !  4) structure factor sfacgq of a (G+q) plane wave is taken into 
 !     account in genmegqblh subroutine; this allows to keep radial integrals
 !     for atom classes only (not for all atoms)
@@ -97,23 +120,20 @@ do igloc=1,ngvecmeloc
               do io2=1,nufr(l2,is)
                 zt1=zzero
                 do l3=0,lmaxexp
-                  do m3=-l3,l3
-                    lm3=idxlm(l3,m3)
-                    zt1=zt1+gnt(lm3,lm2,lm1)*uju(l3,l1,l2,io1,io2)*&
-                      ylmgq(lm3,ig)*dconjg(zi**l3)
-                  enddo !m3
+                  !do m3=-l3,l3
+                  !  lm3=idxlm(l3,m3)
+                  !  zt1=zt1+gnt(lm3,lm2,lm1)*uju(l3,l1,l2,io1,io2)*&
+                  !    ylmgq(lm3,ig)*dconjg(zi**l3)
+                  !enddo !m3
+                  zt1=zt1+zm(l3,lm2,lm1)*uju(l3,l1,l2,io1,io2)
                 enddo !l3
-                zt1=zt1*fourpi
+                !zt1=zt1*fourpi
                 if (abs(zt1).gt.1d-12) then
                   ngntuju(ic,ig)=ngntuju(ic,ig)+1
                   n=ngntuju(ic,ig)
                   gntuju(n,ic,ig)=zt1
                   igntuju(1,n,ic,ig)=lm1+(io1-1)*lmmaxvr
                   igntuju(2,n,ic,ig)=lm2+(io2-1)*lmmaxvr
-                  !igntuju(1,n,ic,ig)=lm1
-                  !igntuju(2,n,ic,ig)=lm2
-                  !igntuju(3,n,ic,ig)=io1
-                  !igntuju(4,n,ic,ig)=io2
                 endif
               enddo !io2
             enddo !io1
@@ -130,7 +150,27 @@ enddo !ig
 !call mpi_grid_barrier(dims=(/dim_k/))
 !call mpi_grid_reduce(ngntuju(1,1),natmcls*ngvecme,dims=(/dim_k/),all=.true.)    
 !call mpi_grid_barrier(dims=(/dim_k/))
-do ig=1,ngvecme
+!do ig=1,ngvecme
+!  call mpi_grid_reduce(gntuju(1,1,ig),ngntujumax*natmcls,dims=(/dim_k/),&
+!    all=.true.)
+!  call mpi_grid_reduce(igntuju(1,1,1,ig),2*ngntujumax*natmcls,dims=(/dim_k/),&
+!    all=.true.)
+!  call mpi_grid_reduce(ngntuju(1,ig),natmcls,dims=(/dim_k/),all=.true.)    
+!  call mpi_grid_barrier(dims=(/dim_k/))
+!enddo
+
+! synchronize blocks of G-vectors arrays
+i=ngvecme/ngvb
+do ig=1,i
+  call mpi_grid_reduce(gntuju(1,1,(ig-1)*ngvb+1),ngvb*ngntujumax*natmcls,&
+    dims=(/dim_k/),all=.true.)
+  call mpi_grid_reduce(igntuju(1,1,1,(ig-1)*ngvb+1),ngvb*2*ngntujumax*natmcls,&
+    dims=(/dim_k/),all=.true.)
+  call mpi_grid_reduce(ngntuju(1,(ig-1)*ngvb+1),ngvb*natmcls,dims=(/dim_k/),&
+    all=.true.)    
+  call mpi_grid_barrier(dims=(/dim_k/))
+enddo
+do ig=i*ngvb+1,ngvecme
   call mpi_grid_reduce(gntuju(1,1,ig),ngntujumax*natmcls,dims=(/dim_k/),&
     all=.true.)
   call mpi_grid_reduce(igntuju(1,1,1,ig),2*ngntujumax*natmcls,dims=(/dim_k/),&
@@ -138,8 +178,10 @@ do ig=1,ngvecme
   call mpi_grid_reduce(ngntuju(1,ig),natmcls,dims=(/dim_k/),all=.true.)    
   call mpi_grid_barrier(dims=(/dim_k/))
 enddo
+
 deallocate(jl)
 deallocate(uju)
 deallocate(gnt)
+deallocate(zm)
 return
 end
