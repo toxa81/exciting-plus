b9c2edfa720ea45dd6704b64cb2b6a5e011b6e7f
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2010-10-01 13:15:04 +0000

    fix in getnghbr.f90

--- branches/elk-1.0.17/src/addons/getnghbr.f90
+++ branches/elk-1.0.17/src/addons/getnghbr.f90
@@ -4,14 +4,21 @@ implicit none
 real(8), intent(in) :: mindist
 real(8), intent(in) :: maxdist
 
-integer i1,i2,i3,ias,ia,is,jas,ja,js,i,n
+integer i1,i2,i3,ias,ia,is,jas,ja,js,i,n,j
 real(8) a(3),v1(3),d1,v2(3),v3(3),dst
 integer llim(3),tmp(5)
+real(8) vrc(3),vrl(3)
+integer ntr(3)
 
 ! find lattice limits
+llim=0
 do i=1,3
-  a(i)=sqrt(avec(1,i)**2+avec(2,i)**2+avec(3,i)**2)
-  llim(i)=int(maxdist/a(i))+1
+  vrc=0.d0
+  vrc(i)=maxdist
+  call getntr(avec,vrc,ntr,vrl)
+  do j=1,3
+    llim(j)=max(abs(ntr(j))+1,llim(j))
+  enddo
 enddo
 
 if (allocated(nnghbr)) deallocate(nnghbr)
@@ -28,11 +35,11 @@ do ias=1,natmtot
     ja=ias2ia(jas)
     js=ias2is(jas)
     do i1=-llim(1),llim(1)
-	  do i2=-llim(2),llim(2)
-	    do i3=-llim(3),llim(3)
-	      v1(:)=atposc(:,ja,js)+i1*avec(:,1)+i2*avec(:,2)+i3*avec(:,3)-&
-	        atposc(:,ia,is)
-	      d1=sqrt(v1(1)**2+v1(2)**2+v1(3)**2)
+      do i2=-llim(2),llim(2)
+        do i3=-llim(3),llim(3)
+          v1(:)=atposc(:,ja,js)+i1*avec(:,1)+i2*avec(:,2)+i3*avec(:,3)-&
+            atposc(:,ia,is)
+          d1=sqrt(v1(1)**2+v1(2)**2+v1(3)**2)
           if (d1.ge.mindist.and.d1.le.maxdist) then
             nnghbr(ias)=nnghbr(ias)+1
             n=nnghbr(ias)
@@ -42,9 +49,9 @@ do ias=1,natmtot
             inghbr(4,n,ias)=i2
             inghbr(5,n,ias)=i3
           endif
-	    enddo !i3
-	  enddo !i2
-	enddo !i1
+        enddo !i3
+      enddo !i2
+    enddo !i1
   enddo !jas
 ! sort by distance    
   do i1=1,nnghbr(ias)-1
