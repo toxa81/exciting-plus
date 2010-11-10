ee6dbc2352a6e07e829179afb8e6fa53f0d1c9f1
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2010-11-10 16:17:29 +0000

    sic fixes

--- src/addons/sic/sic_genvhart.f90
+++ src/addons/sic/sic_genvhart.f90
@@ -11,35 +11,8 @@ real(8) vtrc(3),v2(3),v3(3)
 complex(8), allocatable ::megqwan1(:,:,:)
 complex(8), allocatable :: pwmt(:,:,:)
 complex(8), allocatable :: pwir(:)
-complex(8), allocatable :: ylmtorlm(:,:)
 complex(8) expikt
-allocate(ylmtorlm(lmmaxvr,lmmaxvr))
-
-!b[m1_, m2_] := 
-! If[m1 == 0, 1, 
-!  If[m1 < 0 && m2 < 0, -I/Sqrt[2], 
-!   If[m1 < 0 && m2 > 0, (-1)^m2*I/Sqrt[2], 
-!    If[m1 > 0 && m2 < 0, (-1)^m1/Sqrt[2], 
-!     If[m1 > 0 && m2 > 0, 1/Sqrt[2]]]]]]
-!a[m1_, m2_] := If[Abs[m1] == Abs[m2], b[m1, m2], 0]
-!R[l_, m_, t_, p_] := 
-! Sum[a[m, m1]*SphericalHarmonicY[l, m1, t, p], {m1, -l, l}]
-ylmtorlm=zzero
-do l=0,lmaxvr
-  do m1=-l,l
-    do m2=-l,l
-      if (abs(m1).eq.abs(m2)) then
-        if (m1.eq.0) ylmtorlm(idxlm(l,m1),idxlm(l,m1))=zone
-        if (m1.lt.0.and.m2.lt.0) ylmtorlm(idxlm(l,m1),idxlm(l,m2))=-zi/sqrt(2.d0)
-        if (m1.lt.0.and.m2.gt.0) ylmtorlm(idxlm(l,m1),idxlm(l,m2))=(-1)**m2*zi/sqrt(2.d0)        
-        if (m1.gt.0.and.m2.lt.0) ylmtorlm(idxlm(l,m1),idxlm(l,m2))=(-1)**m1/sqrt(2.d0)        
-        if (m1.gt.0.and.m2.gt.0) ylmtorlm(idxlm(l,m1),idxlm(l,m2))=1.d0/sqrt(2.d0)        
-      endif
-    enddo
-  enddo
-enddo
-! R_{L}=\sum_{L'} M_{L,L'} Y_{L'} so Y_{L}=\sum_{L'} (M^{-1})_{L,L'} R_{L'}
-call invzge(ylmtorlm,lmmaxvr)
+!character*100 qnm
 
 vhwanmt=zzero
 vhwanir=zzero
@@ -49,8 +22,8 @@ call init_q_gq
 ! create q-directories
 !if (mpi_grid_root()) then
 !  call system("mkdir -p q")
-!  do iq=1,nvq0
-!    call getqdir(iq,ivq0m_list(:,iq),qnm)
+!  do iq=1,nvq
+!    call getqdir(iq,vqm(:,iq),qnm)
 !    call system("mkdir -p "//trim(qnm))
 !  enddo
 !endif
@@ -58,6 +31,7 @@ call init_q_gq
 nvqloc=mpi_grid_map(nvq,dim_q)
 allocate(megqwan1(nwann,ngqmax,nvq))
 megqwan1=zzero
+chi0_include_bands(:)=(/100.1d0,-100.1d0/)
 call timer_start(10,reset=.true.)
 ! loop over q-points
 do iqloc=1,nvqloc
@@ -78,7 +52,7 @@ allocate(pwir(ngrtot))
 call timer_start(11,reset=.true.)
 do iq=1,nvq
   do ig=1,ngq(iq)
-    call genpw(vgqc(1,ig,iq),pwmt,pwir,ylmtorlm)
+    call genpw(vgqc(1,ig,iq),pwmt,pwir)
     do itloc=1,ntrloc
       itr=mpi_grid_map(ntr,dim_t,loc=itloc)
       vtrc(:)=vtl(1,itr)*avec(:,1)+vtl(2,itr)*avec(:,2)+vtl(3,itr)*avec(:,3)
@@ -98,6 +72,6 @@ do iq=1,nvq
   enddo !igloc
 enddo !iq
 call timer_stop(11)
-deallocate(pwmt,pwir,megqwan1,ylmtorlm)
+deallocate(pwmt,pwir,megqwan1)
 return
 end
