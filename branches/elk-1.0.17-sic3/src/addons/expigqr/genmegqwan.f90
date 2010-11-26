38b353bd7950c8865cb512b5871824dcb77e9a6b
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2010-11-26 23:39:51 +0000

    sic fixes

--- branches/elk-1.0.17-sic3/src/addons/expigqr/genmegqwan.f90
+++ branches/elk-1.0.17-sic3/src/addons/expigqr/genmegqwan.f90
@@ -29,7 +29,6 @@ if (.not.diag_wan_ibt) then
       if (xloc.eq.mpi_grid_x(dim_b)) then
         ist1=bmegqblh(1,i,ikloc)
         ist2=bmegqblh(2,i,ikloc)
-        zm1=zzero
         do n1=1,nwann
           do n2=1,nwann
             zm1(n1,n2)=dconjg(wanncnrloc(n1,ist1,ikloc))*wann_c_jk(n2,ist2,ikloc)
@@ -50,42 +49,28 @@ if (.not.diag_wan_ibt) then
   deallocate(expkqt)
   deallocate(vtrc)
 else
-  allocate(vtrc(3,nwann))
-  do n=1,nwann
-    j=idxmegqwan(n,n,0,0,0)
-    vtrc(:,n)=avec(:,1)*imegqwan(3,j)+avec(:,2)*imegqwan(4,j)+&
-      avec(:,3)*imegqwan(5,j)
-  enddo
   allocate(zm1(nwann,1))
-  allocate(expkqt(nwann))
   do ikloc=1,nkptnrloc
     ik=mpi_grid_map(nkptnr,dim_k,loc=ikloc)
-    do n=1,nwann
-      expkqt(n)=exp(dcmplx(0.d0,-dot_product(vkcnr(:,ik)+vqc(:,iq),vtrc(:,n))))
-    enddo
     do i1=1,nmegqblhwan(ikloc)
       i=imegqblhwan(i1,ikloc)
       ibloc=mpi_grid_map(nmegqblh(ikloc),dim_b,glob=i,x=xloc)
       if (xloc.eq.mpi_grid_x(dim_b)) then
         ist1=bmegqblh(1,i,ikloc)
         ist2=bmegqblh(2,i,ikloc)
-        zm1=zzero
         do n=1,nwann
           zm1(n,1)=dconjg(wanncnrloc(n,ist1,ikloc))*wann_c_jk(n,ist2,ikloc)
         enddo
         do n=1,nwann
           j=idxmegqwan(n,n,0,0,0)
-          zt2=zm1(n,n)*expkqt(n)
           do ig=1,ngvecme
-            megqwan(j,ig)=megqwan(j,ig)+zt2*megqblh(ibloc,ig,ikloc)
+            megqwan(j,ig)=megqwan(j,ig)+zm1(n,1)*megqblh(ibloc,ig,ikloc)
           enddo !ig 
         enddo !j
       endif !if (xloc.eq.mpi_grid_x(dim_b))
     enddo !i1
   enddo !ikloc
   deallocate(zm1)
-  deallocate(expkqt)
-  deallocate(vtrc)
 endif
 return
 end
