c9a1ed695d3f59b275d21f6c2f7e68e7ca7058d5
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2010-12-17 10:44:25 +0000

    sic total energy fix

--- trunk/src/addons/sic/sic_main.f90
+++ trunk/src/addons/sic/sic_main.f90
@@ -86,9 +86,10 @@ call mpi_grid_reduce(ekin_,dims=(/dim_k/))
 ekin_=ekin_/nkptnr
 if (wproc) then
   write(151,*)
-  etot_=ekin_+0.5d0*engyvcl+engyx+engyc-sic_epot
+  etot_=ekin_+0.5d0*engyvcl+engyx+engyc+engymad-sic_epot
   write(151,'("kinetic energy : ",G18.10)')ekin_
   write(151,'("potential      : ",G18.10)')0.5d0*engyvcl+engyx+engyc
+  write(151,'("madelung       : ",G18.10)')engymad
   write(151,'("sic correction : ",G18.10)')sic_epot
   write(151,'("total energy   : ",G18.10)')etot_
   open(152,file="SIC_ETOT.OUT",form="FORMATTED",status="REPLACE")
