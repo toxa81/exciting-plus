dc0bb99d5bbcf8444db39a7f3035003d787b4ff4
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-01-13 13:45:40 +0000

    fixes and optimizations

--- src/addons/genwffvmt.f90
+++ src/addons/genwffvmt.f90
@@ -7,11 +7,27 @@ integer, intent(in) :: lmmax
 integer, intent(in) :: ngp
 complex(8), intent(in) :: evecfv(nmatmax,nstfv)
 complex(8), intent(in) :: apwalm(ngkmax,apwordmax,lmmaxapw,natmtot)
-complex(8), intent(out) :: wffvmt(nstfv,lmmax,nufrmax,natmtot)
+complex(8), intent(out) :: wffvmt(lmmax,nufrmax,natmtot,nstfv)
 ! local variables
-integer l,m,is,ia,ias,lm,i1,io,ilo
+integer l,m,is,ia,ias,lm,i1,io,ilo,nrow
 integer ordl(0:lmax,natmtot)
-! calculate first-variational coefficients
+complex(8), allocatable :: apwalm_(:,:,:,:)
+complex(8), allocatable :: wffvmt_(:,:,:,:)
+! change order of indexes
+allocate(apwalm_(ngkmax,lmmax,apwordmax,natmtot))
+do ias=1,natmtot
+  do io=1,apwordmax
+    do lm=1,lmmax
+      apwalm_(:,lm,io,ias)=apwalm(:,io,lm,ias)
+    enddo
+  enddo
+enddo
+! precompute \sum_{G} apwalm_(G,lm,io,ias)*evecfv(G,ist)
+allocate(wffvmt_(lmmax,apwordmax,natmtot,nstfv))
+nrow=lmmax*apwordmax*natmtot
+call zgemm('T','N',nrow,nstfv,ngp,zone,apwalm_,ngkmax,evecfv,nmatmax,zzero,&
+  wffvmt_,nrow)
+! get first-variational coefficients
 wffvmt=zzero
 ordl=0
 do is=1,nspecies
@@ -23,8 +39,7 @@ do is=1,nspecies
         ordl(l,ias)=ordl(l,ias)+1
         do m=-l,l
           lm=idxlm(l,m)
-          call zgemv('T',ngp,nstfv,zone,evecfv,nmatmax, &
-            apwalm(1,io,lm,ias),1,zzero,wffvmt(1,lm,ordl(l,ias),ias),1)
+          wffvmt(lm,ordl(l,ias),ias,:)=wffvmt_(lm,ordl(l,ias),ias,:)
         enddo !m
       enddo !io
     enddo !l
@@ -36,11 +51,12 @@ do is=1,nspecies
         do m=-l,l
           lm=idxlm(l,m)
           i1=ngp+idxlo(lm,ilo,ias)
-          wffvmt(:,lm,ordl(l,ias),ias)=evecfv(i1,:)
+          wffvmt(lm,ordl(l,ias),ias,:)=evecfv(i1,:)
         enddo !m
       endif
     enddo !ilo    
   enddo
 enddo
+deallocate(apwalm_,wffvmt_)
 return
 end
