2d610984a02bca7cd9040796c4066676d61ab610
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-01-19 14:33:23 +0000

    continue work on madelk

--- trunk/src/addons/mod_madness.f90
+++ trunk/src/addons/mod_madness.f90
@@ -1,6 +1,10 @@
 module mod_madness
 
 integer m_ngvec
+integer, allocatable :: m_ngknr(:)
 integer, allocatable :: m_igkignr(:,:)
+
+complex(8), allocatable :: m_wann_unkmt(:,:,:,:,:)
+complex(8), allocatable :: m_wann_unkit(:,:,:)
 
 end module
