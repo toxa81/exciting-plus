517132a3dedc66efb9f99f1cfd96e0615138f204
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-01-31 10:46:50 +0000

    formatted output of <W_nk|W_nk>

--- trunk/src/addons/sic/sic_blochsum.f90
+++ trunk/src/addons/sic/sic_blochsum.f90
@@ -5,6 +5,7 @@ implicit none
 integer ik,ikloc,j,n,jas,it,ias,is,ir,itp,ispn
 real(8) x(3)
 real(8), allocatable :: tp(:,:)
+complex(8), allocatable :: zprod(:,:,:)
 complex(8) zt1,zt2,expikt
 complex(8), external :: zfinp_
 !
@@ -15,9 +16,10 @@ s_wvkir=zzero
 
 if (.not.tsic_wv) return
 
+allocate(zprod(2,sic_wantran%nwan,nkpt))
+zprod=zzero
 allocate(tp(2,lmmaxvr))
 call sphcover(lmmaxvr,tp)
-
 do ikloc=1,nkptloc
   ik=mpi_grid_map(nkpt,dim_k,loc=ikloc)
 ! make Bloch sums
@@ -63,9 +65,24 @@ do ikloc=1,nkptloc
       zt2=zt2+zfinp_(s_wvkmt(1,1,1,ispn,j,ikloc),s_wankmt(1,1,1,ispn,j,ikloc),&
         s_wvkir(1,ispn,j,ikloc),s_wankir(1,ispn,j,ikloc))
     enddo
-    write(*,*)"n =",n," <W_nk|W_nk> =",zt1," <(W*V)_nk|W_nk> = ",zt2
+    zprod(1,j,ik)=zt1
+    zprod(2,j,ik)=zt2
   enddo !j
 enddo !ikloc 
+call mpi_grid_reduce(zprod(1,1,1),2*sic_wantran%nwan*nkpt,dims=(/dim_k/))
+if (mpi_grid_root()) then
+  open(210,file="SIC_BLOCHSUM.OUT",form="formatted",status="replace")
+  do ik=1,nkpt
+    write(210,'(" ik : ",I4)')ik
+    do j=1,sic_wantran%nwan
+      n=sic_wantran%iwan(j)
+      write(210,'("  n : ",I4,6X," <W_nk|W_nk> : ",2G18.10)')n,dreal(zprod(1,j,ik)),dimag(zprod(1,j,ik))
+    enddo
+    write(210,*)
+  enddo !ik
+  close(210)
+endif
 deallocate(tp)
+deallocate(zprod)
 return
 end
