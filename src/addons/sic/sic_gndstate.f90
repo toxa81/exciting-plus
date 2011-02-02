d7023d1237f5b85d7e6b6e4e0caf86d624d1568a
a.v.kozhevnikov@gmail.com <a.v.kozhevnikov@gmail.com@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-02-02 15:41:44 +0000

    small fix in sic_gndstate

--- src/addons/sic/sic_gndstate.f90
+++ src/addons/sic/sic_gndstate.f90
@@ -7,12 +7,10 @@ character*3 c3
 sic=.true.
 do isclsic=1,nsclsic
   write(c3,'(I3.3)')isclsic
-  if (mpi_grid_root()) then
-    write(*,'("SIC iteration : ",I4)')isclsic
-  endif
+  if (iproc.eq.0) write(*,'("SIC iteration : ",I4)')isclsic
   call gndstate
   call mpi_world_barrier
-  if (mpi_grid_root()) then
+  if (iproc.eq.0) then
     call system("cp INFO.OUT INFO.OUT"//c3)
     call system("cp EIGVAL.OUT EIGVAL.OUT"//c3)
     call system("cp EFERMI.OUT EFERMI.OUT"//c3)
@@ -20,11 +18,11 @@ do isclsic=1,nsclsic
     call system("cp SIC_WANN_E0.OUT SIC_WANN_E0.OUT"//c3)    
   endif
   call sic_main
-  if (mpi_grid_root()) then
-    call system("cp SIC_ETOT.OUT SIC_ETOT.OUT"//c3)    
-  endif
+!  if (mpi_grid_root()) then
+!    call system("cp SIC_ETOT.OUT SIC_ETOT.OUT"//c3)    
+!  endif
   call mpi_world_barrier
-  if (mpi_grid_root()) then
+  if (iproc.eq.0) then
     call system("cp SIC.OUT SIC.OUT"//c3)  
   endif
 enddo
