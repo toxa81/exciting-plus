29786345b2baaf506eea4833e690588ea3dcfe0f
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-02-14 15:20:55 +0000

    changed to blochsum

--- trunk/src/addons/sic/sic_init.f90
+++ trunk/src/addons/sic/sic_init.f90
@@ -9,7 +9,9 @@ integer i,n,ir,j
 logical texist
 logical, external :: vrinmt,sic_include_cell
 integer itp,lm
+integer i1,i2,i3,ish,vl(3)
 real(8) a,b,x0,tp(2)
+logical l1,l2
 !
 tevecsv=.true.
 lmmaxwan=(lmaxwan+1)**2
@@ -30,53 +32,53 @@ call deletewantran(sic_wantran)
 ! get Wannier transitions
 call genwantran(sic_wantran,-0.d0,sic_me_cutoff,allwt=.true.,waninc=sic_apply)
 
-!if (allocated(sic_orbitals%vtl)) deallocate(sic_orbitals%vtl)
-!allocate(sic_orbitals%vtl(3,sic_maxvtl))
-!sic_orbitals%vtl=-1000000
-!sic_orbitals%ntr=0
-!l2=.true.
-!ish=0
-!do while (l2)
-!  l1=.false.
-!  do i1=-ish,ish
-!    do i2=-ish,ish
-!      do i3=-ish,ish
-!        if (abs(i1).eq.ish.or.abs(i2).eq.ish.or.abs(i3).eq.ish) then
-!          vl=(/i1,i2,i3/)
-!          if (sic_include_cell(vl)) then
-!            l1=.true.            
-!            sic_orbitals%ntr=sic_orbitals%ntr+1
-!            if (sic_orbitals%ntr.gt.sic_maxvtl) then
-!              write(*,'("Error(sic_init) : sic_maxvtl is too small")')
-!              call pstop
-!            endif
-!            sic_orbitals%vtl(:,sic_orbitals%ntr)=vl
-!          endif
-!        endif
-!      enddo
-!    enddo
-!  enddo !i1
-!  if (l1) then
-!    ish=ish+1
-!  else
-!    l2=.false.
-!  endif
-!enddo
-!! Cartesian coordinates of translation vectors
-!if (allocated(sic_orbitals%vtc)) deallocate(sic_orbitals%vtc)
-!allocate(sic_orbitals%vtc(3,sic_orbitals%ntr))
-!do i=1,sic_orbitals%ntr
-!  sic_orbitals%vtc(:,i)=sic_orbitals%vtl(1,i)*avec(:,1)+&
-!                        sic_orbitals%vtl(2,i)*avec(:,2)+&
-!                        sic_orbitals%vtl(3,i)*avec(:,3)
-!end do
-! find translation limits
+if (allocated(sic_orbitals%vtl)) deallocate(sic_orbitals%vtl)
+allocate(sic_orbitals%vtl(3,sic_maxvtl))
+sic_orbitals%vtl=-1000000
+sic_orbitals%ntr=0
+l2=.true.
+ish=0
+do while (l2)
+  l1=.false.
+  do i1=-ish,ish
+    do i2=-ish,ish
+      do i3=-ish,ish
+        if (abs(i1).eq.ish.or.abs(i2).eq.ish.or.abs(i3).eq.ish) then
+          vl=(/i1,i2,i3/)
+          if (sic_include_cell(vl)) then
+            l1=.true.            
+            sic_orbitals%ntr=sic_orbitals%ntr+1
+            if (sic_orbitals%ntr.gt.sic_maxvtl) then
+              write(*,'("Error(sic_init) : sic_maxvtl is too small")')
+              call pstop
+            endif
+            sic_orbitals%vtl(:,sic_orbitals%ntr)=vl
+          endif
+        endif
+      enddo
+    enddo
+  enddo !i1
+  if (l1) then
+    ish=ish+1
+  else
+    l2=.false.
+  endif
+enddo
+! Cartesian coordinates of translation vectors
+if (allocated(sic_orbitals%vtc)) deallocate(sic_orbitals%vtc)
+allocate(sic_orbitals%vtc(3,sic_orbitals%ntr))
+do i=1,sic_orbitals%ntr
+  sic_orbitals%vtc(:,i)=sic_orbitals%vtl(1,i)*avec(:,1)+&
+                        sic_orbitals%vtl(2,i)*avec(:,2)+&
+                        sic_orbitals%vtl(3,i)*avec(:,3)
+end do
+!! find translation limits
 !sic_orbitals%tlim=0
 !do i=1,3
 !  sic_orbitals%tlim(1,i)=minval(sic_orbitals%vtl(i,1:sic_orbitals%ntr))
 !  sic_orbitals%tlim(2,i)=maxval(sic_orbitals%vtl(i,1:sic_orbitals%ntr))
 !enddo
-! find mapping from translations to linear index
+!! find mapping from translations to linear index
 !if (allocated(sic_orbitals%ivtit)) deallocate(sic_orbitals%ivtit)
 !allocate(sic_orbitals%ivtit(sic_orbitals%tlim(1,1):sic_orbitals%tlim(2,1),&
 !                            sic_orbitals%tlim(1,2):sic_orbitals%tlim(2,2),&
@@ -86,6 +88,8 @@ call genwantran(sic_wantran,-0.d0,sic_me_cutoff,allwt=.true.,waninc=sic_apply)
 !  sic_orbitals%ivtit(sic_orbitals%vtl(1,i),sic_orbitals%vtl(2,i),&
 !    sic_orbitals%vtl(3,i))=i
 !enddo
+
+call sic_genrmesh
 
 if (allocated(s_tpw)) deallocate(s_tpw)
 allocate(s_tpw(s_ntp))
@@ -99,10 +103,6 @@ if (allocated(s_ylmb)) deallocate(s_ylmb)
 allocate(s_ylmb(s_ntp,lmmaxwan))
 if (allocated(s_spx)) deallocate(s_spx)
 allocate(s_spx(3,s_ntp))
-if (allocated(s_r)) deallocate(s_r)
-allocate(s_r(s_nr))
-if (allocated(s_rw)) deallocate(s_rw)
-allocate(s_rw(s_nr))
 if (allocated(s_wanlm)) deallocate(s_wanlm)
 allocate(s_wanlm(lmmaxwan,s_nr,nspinor,sic_wantran%nwan))
 if (allocated(s_wvlm)) deallocate(s_wvlm)
@@ -135,14 +135,15 @@ if (allocated(sic_wvuy)) deallocate(sic_wvuy)
 allocate(sic_wvuy(lmmaxvr,nufrmax,natmtot,sic_wantran%nwan,nspinor,nkptloc))
 sic_wvuy=zzero
 
-!if (allocated(s_wankmt)) deallocate(s_wankmt)
-!allocate(s_wankmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
-!if (allocated(s_wankir)) deallocate(s_wankir)
-!allocate(s_wankir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
-!if (allocated(s_wvkmt)) deallocate(s_wvkmt)
-!allocate(s_wvkmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
-!if (allocated(s_wvkir)) deallocate(s_wvkir)
-!allocate(s_wvkir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
+if (allocated(s_wankmt)) deallocate(s_wankmt)
+allocate(s_wankmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
+if (allocated(s_wankir)) deallocate(s_wankir)
+allocate(s_wankir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
+if (allocated(s_wvkmt)) deallocate(s_wvkmt)
+allocate(s_wvkmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
+if (allocated(s_wvkir)) deallocate(s_wvkir)
+allocate(s_wvkir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
+
 inquire(file="SIC_WANN_E0.OUT",exist=texist)
 if (texist) then
   open(170,file="SIC_WANN_E0.OUT",form="FORMATTED",status="OLD")
@@ -165,8 +166,8 @@ do itp=1,s_ntp
   do lm=1,lmmaxwan
     s_rlmb(itp,lm)=s_rlmf(lm,itp)*s_tpw(itp)
     s_ylmb(itp,lm)=dconjg(s_ylmf(lm,itp))*s_tpw(itp) 
-  enddo  
-enddo 
+  enddo
+enddo
 !do i=4,10
 !  call test1(6,i)
 !  call test1(14,i)
@@ -190,20 +191,6 @@ enddo
 !enddo
 !call bstop
 
-! generate radial mesh
-x0=1d-6
-b=log(sic_wan_cutoff/x0)/(s_nr-1)
-a=x0/exp(b)
-do ir=1,s_nr
-  s_r(ir)=a*exp(ir*b)
-enddo
-! generate radial weights for integration
-s_rw=0.d0
-do ir=1,s_nr-1
-  s_rw(ir)=s_rw(ir)+0.5d0*(s_r(ir+1)-s_r(ir))*s_r(ir)**2
-  s_rw(ir+1)=s_rw(ir+1)+0.5d0*(s_r(ir+1)-s_r(ir))*s_r(ir+1)**2
-enddo
-
 if (mpi_grid_root()) then
 ! size of sperical arrays
   a=2*16.d0*lmmaxwan*s_nr*nspinor*sic_wantran%nwan/1024/1024
@@ -215,32 +202,33 @@ endif
 return
 end
 
-!logical function sic_include_cell(vl)
-!use modmain
-!use mod_sic
-!implicit none
-!integer, intent(in) :: vl(3)
-!logical l1
-!integer n,ias,jas,ir,j
-!real(8) vt(3),v1(3)
-!vt(:)=vl(1)*avec(:,1)+vl(2)*avec(:,2)+vl(3)*avec(:,3)
-!l1=.false.
-!do j=1,sic_wantran%nwan
-!  n=sic_wantran%iwan(j)
-!  ias=wan_info(1,n)
-!  do jas=1,natmtot
-!    v1(:)=atposc(:,ias2ia(jas),ias2is(jas))+vt(:)-&
-!      atposc(:,ias2ia(ias),ias2is(ias))
-!    if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
-!  enddo
-!  do ir=1,ngrtot
-!    v1(:)=vgrc(:,ir)+vt(:)-atposc(:,ias2ia(ias),ias2is(ias))
-!    if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
-!  enddo
-!enddo
-!sic_include_cell=l1
-!return
-!end
+logical function sic_include_cell(vl)
+use modmain
+use mod_sic
+implicit none
+integer, intent(in) :: vl(3)
+logical l1
+integer n,ias,jas,ir,j
+real(8) vt(3),v1(3)
+vt(:)=vl(1)*avec(:,1)+vl(2)*avec(:,2)+vl(3)*avec(:,3)
+l1=.false.
+do j=1,sic_wantran%nwan
+  n=sic_wantran%iwan(j)
+  ias=wan_info(1,n)
+  do jas=1,natmtot
+    v1(:)=atposc(:,ias2ia(jas),ias2is(jas))+vt(:)-&
+      atposc(:,ias2ia(ias),ias2is(ias))
+    if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
+  enddo
+  do ir=1,ngrtot
+    v1(:)=vgrc(:,ir)+vt(:)-atposc(:,ias2ia(ias),ias2is(ias))
+    if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
+  enddo
+enddo
+sic_include_cell=l1
+return
+end
+
 !
 !subroutine test1(ntp,lmax)
 !use modmain
