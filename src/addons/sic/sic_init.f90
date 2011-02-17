e1bb1df3ed3571bcc0538252c4efbf97a948f4ca
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-02-17 14:12:59 +0000

    minor changes

--- src/addons/sic/sic_init.f90
+++ src/addons/sic/sic_init.f90
@@ -90,23 +90,16 @@ end do
 !enddo
 
 call sic_genrmesh
+call sic_gensmesh
 
-if (allocated(s_tpw)) deallocate(s_tpw)
-allocate(s_tpw(s_ntp))
-if (allocated(s_rlmf)) deallocate(s_rlmf)
-allocate(s_rlmf(lmmaxwan,s_ntp))
-if (allocated(s_ylmf)) deallocate(s_ylmf)
-allocate(s_ylmf(lmmaxwan,s_ntp))
-if (allocated(s_rlmb)) deallocate(s_rlmb)
-allocate(s_rlmb(s_ntp,lmmaxwan))
-if (allocated(s_ylmb)) deallocate(s_ylmb)
-allocate(s_ylmb(s_ntp,lmmaxwan))
-if (allocated(s_spx)) deallocate(s_spx)
-allocate(s_spx(3,s_ntp))
+!call sic_test_mesh
+
 if (allocated(s_wanlm)) deallocate(s_wanlm)
 allocate(s_wanlm(lmmaxwan,s_nr,nspinor,sic_wantran%nwan))
+s_wanlm=zzero
 if (allocated(s_wvlm)) deallocate(s_wvlm)
 allocate(s_wvlm(lmmaxwan,s_nr,nspinor,sic_wantran%nwan))
+s_wvlm=zzero
 if (allocated(vwanme)) deallocate(vwanme)
 allocate(vwanme(sic_wantran%nwt))
 vwanme=zzero
@@ -135,14 +128,14 @@ if (allocated(sic_wvuy)) deallocate(sic_wvuy)
 allocate(sic_wvuy(lmmaxvr,nufrmax,natmtot,sic_wantran%nwan,nspinor,nkptloc))
 sic_wvuy=zzero
 
-if (allocated(s_wankmt)) deallocate(s_wankmt)
-allocate(s_wankmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
-if (allocated(s_wankir)) deallocate(s_wankir)
-allocate(s_wankir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
-if (allocated(s_wvkmt)) deallocate(s_wvkmt)
-allocate(s_wvkmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
-if (allocated(s_wvkir)) deallocate(s_wvkir)
-allocate(s_wvkir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
+!if (allocated(s_wankmt)) deallocate(s_wankmt)
+!allocate(s_wankmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
+!if (allocated(s_wankir)) deallocate(s_wankir)
+!allocate(s_wankir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
+!if (allocated(s_wvkmt)) deallocate(s_wvkmt)
+!allocate(s_wvkmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan,nkptloc))
+!if (allocated(s_wvkir)) deallocate(s_wvkir)
+!allocate(s_wvkir(ngrtot,nspinor,sic_wantran%nwan,nkptloc))
 
 inquire(file="SIC_WANN_E0.OUT",exist=texist)
 if (texist) then
@@ -155,42 +148,6 @@ if (texist) then
 20 sic_wann_e0=0.d0
 endif
 30 continue
-! Lebedev-Laikov mesh
-call leblaik(s_ntp,s_spx,s_tpw)
-! get (theta,phi) of each spx vector and generate spherical harmonics
-do itp=1,s_ntp
-  s_tpw(itp)=s_tpw(itp)*fourpi
-  call sphcrd(s_spx(:,itp),a,tp)
-  call genrlm(lmaxwan,tp,s_rlmf(1,itp))
-  call genylm(lmaxwan,tp,s_ylmf(1,itp))
-  do lm=1,lmmaxwan
-    s_rlmb(itp,lm)=s_rlmf(lm,itp)*s_tpw(itp)
-    s_ylmb(itp,lm)=dconjg(s_ylmf(lm,itp))*s_tpw(itp) 
-  enddo
-enddo
-!do i=4,10
-!  call test1(6,i)
-!  call test1(14,i)
-!  call test1(26,i)
-!  call test1(38,i)
-!  call test1(50,i)
-!  call test1(74,i)
-!  call test1(110,i)
-!  call test1(146,i) 
-!  call test1(170,i)
-!  call test1(194,i)
-!  call test1(230,i)
-!  call test1(266,i)
-!  call test1(302,i)
-!  call test1(350,i)
-!  call test1(434,i)
-!  call test1(590,i)
-!  call test1(770,i)
-!  call test1(974,i)
-!  call test1(1202,i)
-!enddo
-!call bstop
-
 if (mpi_grid_root()) then
 ! size of sperical arrays
   a=2*16.d0*lmmaxwan*s_nr*nspinor*sic_wantran%nwan/1024/1024
@@ -208,20 +165,20 @@ use mod_sic
 implicit none
 integer, intent(in) :: vl(3)
 logical l1
-integer n,ias,jas,ir,j
+integer n,jas,ias,ir,j
 real(8) vt(3),v1(3)
 vt(:)=vl(1)*avec(:,1)+vl(2)*avec(:,2)+vl(3)*avec(:,3)
 l1=.false.
 do j=1,sic_wantran%nwan
   n=sic_wantran%iwan(j)
-  ias=wan_info(1,n)
-  do jas=1,natmtot
-    v1(:)=atposc(:,ias2ia(jas),ias2is(jas))+vt(:)-&
-      atposc(:,ias2ia(ias),ias2is(ias))
-    if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
-  enddo
+  jas=wan_info(1,n)
+!  do ias=1,natmtot
+!    v1(:)=atposc(:,ias2ia(ias),ias2is(ias))+vt(:)-&
+!      atposc(:,ias2ia(jas),ias2is(jas))
+!    if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
+!  enddo
   do ir=1,ngrtot
-    v1(:)=vgrc(:,ir)+vt(:)-atposc(:,ias2ia(ias),ias2is(ias))
+    v1(:)=vgrc(:,ir)+vt(:)-atposc(:,ias2ia(jas),ias2is(jas))
     if (sqrt(sum(v1(:)**2)).le.sic_wan_cutoff) l1=.true.
   enddo
 enddo
@@ -229,55 +186,4 @@ sic_include_cell=l1
 return
 end
 
-!
-!subroutine test1(ntp,lmax)
-!use modmain
-!implicit none
-!integer, intent(in) :: ntp
-!integer, intent(in) :: lmax
-!integer itp,lm,lmmax,itp1
-!real(8) a,tp(2)
-!complex(8) z1
-!real(8), allocatable :: spx(:,:)
-!real(8), allocatable :: tpw(:)
-!complex(8), allocatable :: ylmf(:,:)
-!complex(8), allocatable :: ylmb(:,:)
-!
-!lmmax=(lmax+1)**2
-!
-!allocate(spx(3,ntp))
-!allocate(tpw(ntp))
-!allocate(ylmf(lmmax,ntp))
-!allocate(ylmb(ntp,lmmax))
-!! Lebedev-Laikov mesh
-!call leblaik(ntp,spx,tpw)
-!! get (theta,phi) of each spx vector and generate spherical harmonics
-!do itp=1,ntp                   
-!  tpw(itp)=tpw(itp)*fourpi
-!  call sphcrd(spx(1,itp),a,tp)
-!  call genylm(lmax,tp,ylmf(1,itp))
-!  do lm=1,lmmax
-!    ylmb(itp,lm)=dconjg(ylmf(lm,itp))*tpw(itp) 
-!  enddo  
-!enddo 
-!a=0.d0
-!do itp=1,ntp
-!  do itp1=1,ntp
-!    z1=zzero
-!    do lm=1,lmmax
-!      z1=z1+ylmb(itp,lm)*ylmf(lm,itp1)
-!    enddo
-!    !if (itp.eq.itp1) z1=z1-zone
-!    !a=max(a,abs(z1))
-!    if (itp.ne.itp1) a=max(a,abs(z1))
-!  enddo
-!enddo
-!if (mpi_grid_root()) then
-!  write(*,'("[test1] l = ",I4," ntp = ",I4)')lmax,ntp
-!  write(*,'("[test1] Lebedev quadrature completeness error : ",G18.10)')a
-!  write(*,*)
-!endif
-!deallocate(spx,tpw,ylmf,ylmb)
-!return
-!end
 
