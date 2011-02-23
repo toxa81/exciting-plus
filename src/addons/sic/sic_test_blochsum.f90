e1be4fd6cc5ef7547419dd9a16889751358a6c39
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-02-23 13:32:18 +0000

    temporary commit to check bloch expansions

--- src/addons/sic/sic_test_blochsum.f90
+++ src/addons/sic/sic_test_blochsum.f90
@@ -1,12 +1,12 @@
-subroutine sic_blochsum
+subroutine sic_test_blochsum
 use modmain
 use mod_sic
 implicit none
 integer ik,ikloc,j,n,jas,it,ias,is,ir,itp,ispn,ntrloc,itloc,lm
-real(8) x(3)
+real(8) x(3),t1
 real(8), allocatable :: tp(:,:)
 complex(8), allocatable :: zprod(:,:,:)
-complex(8) zt1,zt2,expikt
+complex(8) zt1,zt2,expikt,wanval(nspinor)
 complex(8), external :: zfinp_
 complex(8), allocatable :: wfmt(:,:)
 complex(8), allocatable :: wftp(:,:,:)
@@ -17,20 +17,20 @@ s_wankir=zzero
 s_wvkmt=zzero
 s_wvkir=zzero
 
-if (.not.tsic_wv) return
 
 allocate(zprod(2,sic_wantran%nwan,nkpt))
 allocate(wfmt(lmmaxvr,nrmtmax))
 allocate(wftp(s_ntp,nrmtmax,nspinor))
+!allocate(wftp(lmmaxvr,nrmtmax,nspinor))
 allocate(wflm(lmmaxvr,nrmtmax,nspinor))
-
 
 
 zprod=zzero
 allocate(tp(2,lmmaxvr))
 call sphcover(lmmaxvr,tp)
+
 ntrloc=mpi_grid_map(sic_orbitals%ntr,dim2)
-do ikloc=1,nkptloc
+do ikloc=1,1 !nkptloc
   ik=mpi_grid_map(nkpt,dim_k,loc=ikloc)
 ! make Bloch sums
   do j=1,sic_wantran%nwan
@@ -43,12 +43,24 @@ do ikloc=1,nkptloc
         it=mpi_grid_map(sic_orbitals%ntr,dim2,loc=itloc)
         expikt=exp(-zi*dot_product(vkc(:,ik),sic_orbitals%vtc(:,it)))
 ! muffin-tins
+!        do ir=1,nrmt(is)
+!          do itp=1,lmmaxvr
+!            x(:)=(/sin(tp(1,itp))*cos(tp(2,itp)),&
+!                   sin(tp(1,itp))*sin(tp(2,itp)),&
+!                   cos(tp(1,itp))/)*spr(ir,is)+&
+!                   atposc(:,ias2ia(ias),ias2is(ias))+sic_orbitals%vtc(:,it)
+!            call s_get_wanval(n,x,wanval)
+!            wftp(itp,ir,:)=wftp(itp,ir,:)+wanval(:)*expikt
+!          enddo !itp
+!        enddo !ir
         do ir=1,nrmt(is)
           do itp=1,s_ntp
             x(:)=s_spx(:,itp)*spr(ir,is)+atposc(:,ias2ia(ias),ias2is(ias))+&
-                 sic_orbitals%vtc(:,it)-atposc(:,ias2ia(jas),ias2is(jas))
+                 sic_orbitals%vtc(:,it) !-atposc(:,ias2ia(jas),ias2is(jas))
             do ispn=1,nspinor
-              wftp(itp,ir,ispn)=wftp(itp,ir,ispn)+expikt*s_func_val(x,s_wanlm(1,1,ispn,j)) 
+              !wftp(itp,ir,ispn)=wftp(itp,ir,ispn)+expikt*s_func_val(x,s_wanlm(1,1,ispn,j)) 
+              call s_get_wanval(n,x,wanval)
+              wftp(itp,ir,:)=wftp(itp,ir,:)+wanval(:)*expikt
               !s_wankmt(itp,ir,ias,ispn,j,ikloc)=s_wankmt(itp,ir,ias,ispn,j,ikloc)+&
               !  expikt*s_func_val(x,s_wanlm(1,1,ispn,j))
               !s_wvkmt(itp,ir,ias,ispn,j,ikloc)=s_wvkmt(itp,ir,ias,ispn,j,ikloc)+&
@@ -57,26 +69,32 @@ do ikloc=1,nkptloc
           enddo !itp
         enddo !ir
       enddo !itloc
-      if (ik.eq.1) write(*,*)"image part of wnk",sum(abs(dimag(wftp)))/s_ntp/nrmtmax
  ! convert to spherical harmonics
       do ispn=1,nspinor
         call zgemm('T','N',lmmaxvr,nrmt(is),s_ntp,zone,s_ylmb,s_ntp,&
           wftp(1,1,ispn),s_ntp,zzero,wflm(1,1,ispn),lmmaxvr)
-        s_wankmt(:,:,ias,ispn,j,ikloc)=wflm(:,:,ispn)
+          s_wankmt(:,:,ias,ispn,j,ikloc)=wflm(:,:,ispn)
       enddo !ispn
+      !do ispn=1,nspinor
+      !  call zgemm('N','N',lmmaxvr,nrmt(is),lmmaxvr,zone,zfshtvr,lmmaxvr, &
+      !    wftp(1,1,ispn),lmmaxvr,zzero,s_wankmt(1,1,ias,ispn,j,ikloc),lmmaxvr)
+      !enddo
     enddo !ias
 ! interstitial
     do itloc=1,ntrloc
       it=mpi_grid_map(sic_orbitals%ntr,dim2,loc=itloc)
       expikt=exp(-zi*dot_product(vkc(:,ik),sic_orbitals%vtc(:,it)))
       do ir=1,ngrtot
-        x(:)=vgrc(:,ir)+sic_orbitals%vtc(:,it)-atposc(:,ias2ia(jas),ias2is(jas))
-        do ispn=1,nspinor
-          s_wankir(ir,ispn,j,ikloc)=s_wankir(ir,ispn,j,ikloc)+&
-            s_func_val(x,s_wanlm(1,1,ispn,j))*expikt 
-          s_wvkir(ir,ispn,j,ikloc)=s_wvkir(ir,ispn,j,ikloc)+&
-            s_func_val(x,s_wvlm(1,1,ispn,j))*expikt 
-        enddo
+        x(:)=vgrc(:,ir)+sic_orbitals%vtc(:,it)
+        call s_get_wanval(n,x,wanval)
+        s_wankir(ir,:,j,ikloc)=s_wankir(ir,:,j,ikloc)+wanval(:)*expikt
+        !x(:)=vgrc(:,ir)+sic_orbitals%vtc(:,it)-atposc(:,ias2ia(jas),ias2is(jas))
+        !do ispn=1,nspinor
+        !  s_wankir(ir,ispn,j,ikloc)=s_wankir(ir,ispn,j,ikloc)+&
+        !    s_func_val(x,s_wanlm(1,1,ispn,j))*expikt 
+        !  s_wvkir(ir,ispn,j,ikloc)=s_wvkir(ir,ispn,j,ikloc)+&
+        !    s_func_val(x,s_wvlm(1,1,ispn,j))*expikt 
+        !enddo
       enddo
     enddo !itloc
     call mpi_grid_reduce(s_wankmt(1,1,1,1,j,ikloc),&
@@ -95,16 +113,20 @@ do ikloc=1,nkptloc
 !        s_wankmt(:,:,ias,ispn,j,ikloc)=wfmt
 !      enddo
 !    enddo
-    if (ikloc.eq.1) then
-    do lm=1,lmmaxvr
-    do ir=1,nrmt(1)
-      write(100+j,*)spr(ir,1),dreal(s_wankmt(lm,ir,1,1,j,1))
-      write(200+j,*)spr(ir,1),dimag(s_wankmt(lm,ir,1,1,j,1))
-    enddo
-    write(100+j,*)
-    write(200+j,*)
-    enddo
-    endif
+    if (mpi_grid_root((/dim2/)).and.ik.eq.1.and.j.eq.1) then
+      open(220,file="re_wnkmt.dat",form="formatted",status="replace")
+      open(221,file="im_wnkmt.dat",form="formatted",status="replace")
+      do lm=1,lmmaxvr
+        do ir=1,nrmt(1)
+          write(220,'(2G18.10)')spr(ir,1),dreal(s_wankmt(lm,ir,1,1,1,1)) 
+          write(221,'(2G18.10)')spr(ir,1),dimag(s_wankmt(lm,ir,1,1,1,1)) 
+        enddo
+        write(220,*)
+        write(221,*)
+      enddo
+      close(220)
+      close(221)
+    endif 
  
     zt1=zzero
     zt2=zzero
--- src/addons/sic/sic_test_fvprj.f90
+++ src/addons/sic/sic_test_fvprj.f90
@@ -4,7 +4,8 @@ use mod_sic
 use mod_nrkp
 implicit none
 integer, intent(in) :: fout
-integer ikloc,ik,ist,ir,itp,i,j,n,ispn,istsv
+integer ikloc,ik,ist,ir,itp,i,j,n,ispn,istsv,lm,io,ig,ias
+integer io1,io2
 real(8) vrc(3),t1
 complex(8) zt1,zt2
 complex(8), allocatable :: apwalm(:,:,:,:)
@@ -12,71 +13,262 @@ complex(8), allocatable :: wffvmt(:,:,:,:)
 complex(8), allocatable :: evecfvnr(:,:,:)
 complex(8), allocatable :: evecsvnr(:,:)
 complex(8), allocatable :: wffvtp(:,:)
+complex(8), allocatable :: wantp(:,:)
 complex(8), allocatable :: wffvlm(:,:)
-complex(8), external :: zdotc
+complex(8), allocatable :: wfmt(:,:)
+complex(8), allocatable :: fvmt(:,:,:)
+complex(8), allocatable :: fvir(:)
+complex(8), allocatable :: wnkmt(:,:,:,:,:)
+complex(8), allocatable :: wnkir(:,:,:)
+      
+complex(8), external :: zdotc,zfinp_
 !
 allocate(apwalm(ngkmax,apwordmax,lmmaxapw,natmtot))
-allocate(wffvmt(lmmaxvr,nufrmax,natmtot,nstfv))
+!allocate(wffvmt(lmmaxvr,nufrmax,natmtot,nstfv))
 allocate(evecfvnr(nmatmax,nstfv,nspnfv))
 allocate(evecsvnr(nstsv,nstsv))
-allocate(wffvtp(s_ntp,s_nr))
-allocate(wffvlm(lmmaxwan,s_nr))
-open(220,file="sic_test_fvprj.out",form="formatted",status="replace")
-t1=0.d0
-do ikloc=1,nkptnrloc
+!allocate(wffvtp(s_ntp,s_nr))
+!allocate(wantp(s_ntp,s_nr))
+!allocate(wffvlm(lmmaxwan,s_nr))
+allocate(wfmt(lmmaxvr,nrmtmax))
+!allocate(fvmt(lmmaxvr,nrmtmax,natmtot))
+!allocate(fvir(ngrtot))
+allocate(wnkmt(lmmaxvr,nrmtmax,natmtot,nspinor,sic_wantran%nwan))
+allocate(wnkir(ngrtot,nspinor,sic_wantran%nwan))
+  
+!open(220,file="sic_test_fvprj.out",form="formatted",status="replace")
+! == test1: W_{nk} is analytical, integration is numerical ==
+!write(220,'("test1")')
+do ikloc=1,1!nkptnrloc
   ik=mpi_grid_map(nkptnr,dim_k,loc=ikloc)
-  write(220,'("ik : ",I4,"  vklnr : ",3G18.10)')ik,vklnr(:,ik)
+  !write(220,'("ik : ",I4,"  vklnr : ",3G18.10)')ik,vklnr(:,ik)
   call getevecfv(vklnr(1,ik),vgklnr(1,1,ikloc),evecfvnr)
   call getevecsv(vklnr(1,ik),evecsvnr)
   call match(ngknr(ikloc),gknr(1,ikloc),tpgknr(1,1,ikloc),&
     sfacgknr(1,1,ikloc),apwalm)
-  call genwffvmt(lmaxvr,lmmaxvr,ngknr(ikloc),evecfvnr,apwalm,wffvmt)
-  do ist=1,nstfv
-    write(220,'("  ist : ",I4)')ist
-    do ir=1,s_nr
-      do itp=1,s_ntp
-        vrc(:)=s_spx(:,itp)*s_r(ir)
-        call s_get_wffvval(vrc,ngknr(ikloc),vkcnr(1,ik),vgkcnr(1,1,ikloc), &
-          wffvmt(1,1,1,ist),evecfvnr(1,ist,1),wffvtp(itp,ir))
+  wnkmt=zzero
+  wnkir=zzero
+  do j=1,sic_wantran%nwan
+    n=sic_wantran%iwan(j)
+    do ispn=1,nspinor
+      do ias=1,natmtot
+        wfmt=zzero
+        do ir=1,nrmt(ias2is(ias))
+          do lm=1,lmmaxvr
+            do io=1,nufr(lm2l(lm),ias2is(ias))
+              wfmt(lm,ir)=wfmt(lm,ir)+wann_unkmt(lm,io,ias,ispn,n,ikloc)*&
+                ufr(ir,lm2l(lm),io,ias2ic(ias))
+            enddo
+          enddo !lm
+        enddo !ir
+        !call zgemm('N','N',lmmaxvr,nrmt(ias2is(ias)),lmmaxvr,zone,zbshtvr,&
+        !  lmmaxvr,wfmt,lmmaxvr,zzero,wnkmt(1,1,ias,ispn,j),lmmaxvr)
+        wnkmt(:,:,ias,ispn,j)=wfmt(:,:)
+      enddo !ias
+      do ig=1,ngknr(ikloc)
+        wnkir(igfft(igkignr(ig,ikloc)),ispn,j)=wann_unkit(ig,ispn,n,ikloc)/&
+          sqrt(omega)
       enddo
+      call zfftifc(3,ngrid,1,wnkir(1,ispn,j))
+    enddo !ispn
+    zt1=zzero
+    do ispn=1,nspinor
+      zt1=zt1+zfinp_(wnkmt(1,1,1,ispn,j),wnkmt(1,1,1,ispn,j),wnkir(1,ispn,j),&
+        wnkir(1,ispn,j))
     enddo
-    call zgemm('T','N',lmmaxwan,s_nr,s_ntp,zone,s_ylmb,s_ntp,wffvtp,&
-      s_ntp,zzero,wffvlm,lmmaxwan)
-    do j=1,sic_wantran%nwan
-      n=sic_wantran%iwan(j)
-      do ispn=1,nspinor
-        zt1=zzero
-        do ir=1,s_nr
-          zt1=zt1+zdotc(lmmaxwan,s_wanlm(1,ir,ispn,j),1,wffvlm(1,ir),1)*s_rw(ir)
+    write(*,*)" n : ",n,"   norm : ",zt1
+    !write(220,'(" n : ",I4,"   <W_nk|W_nk> : ",2G18.10)')n,dreal(zt1),dimag(zt1)
+    if (mpi_grid_root((/dim2/)).and.ik.eq.1.and.j.eq.1) then
+      open(220,file="re_wnkmt_exact.dat",form="formatted",status="replace")
+      open(221,file="im_wnkmt_exact.dat",form="formatted",status="replace")
+      do lm=1,lmmaxvr
+        do ir=1,nrmt(1)
+          write(220,'(2G18.10)')spr(ir,1),dreal(wnkmt(lm,ir,1,1,1))
+          write(221,'(2G18.10)')spr(ir,1),dimag(wnkmt(lm,ir,1,1,1))
         enddo
-        zt2=zzero
-        istsv=ist+(ispn-1)*nstfv
-        do i=1,nstsv
-          zt2=zt2+dconjg(wanncnrloc(n,i,ikloc)*evecsvnr(istsv,i))
-        enddo !i
-        t1=max(t1,abs(zt1-zt2))
-        write(220,'("    n : ",I4,"  ispn : ",I4,&
-          &"  numerical : ",2G18.10,"  analytical : ",2G18.10,&
-          &"  diff : ",G18.10)')n,ispn,dreal(zt1),dimag(zt1),&
-          dreal(zt2),dimag(zt2),abs(zt1-zt2)
-      enddo !ispn
-    enddo !j
-  enddo !ist
-  write(220,*)
-  call flushifc(220)
+        write(220,*)
+        write(221,*)
+      enddo
+      close(220)
+      close(221)
+    endif
+  enddo !j
+
+!  do ist=1,nstfv
+!    write(220,'("  ist : ",I4)')ist
+!    fvmt=zzero
+!    fvir=zzero
+!! generate first-variational wave function
+!    do ias=1,natmtot
+!      call wavefmt(1,lmaxvr,ias2is(ias),ias2ia(ias),ngknr(ikloc),apwalm,&
+!        evecfvnr(1,ist,1),lmmaxvr,wfmt)
+!! convert to spherical coordinates
+!      !call zgemm('N','N',lmmaxvr,nrmt(ias2is(ias)),lmmaxvr,zone,zbshtvr,&
+!      !  lmmaxvr,wfmt,lmmaxvr,zzero,fvmt(1,1,ias),lmmaxvr)
+!      fvmt(:,:,ias)=wfmt(:,:)
+!    enddo
+!    do ig=1,ngknr(ikloc)
+!      fvir(igfft(igkignr(ig,ikloc)))=evecfvnr(ig,ist,1)/sqrt(omega)
+!    enddo
+!    call zfftifc(3,ngrid,1,fvir)
+!    zt1=zfinp_(fvmt,fvmt,fvir,fvir)
+!    write(220,'("  norm : ",2G18.10)')dreal(zt1),dimag(zt1) 
+!    do j=1,sic_wantran%nwan
+!      n=sic_wantran%iwan(j)
+!      do ispn=1,nspinor
+!        zt1=zfinp_(wnkmt(1,1,1,ispn,j),fvmt,wnkir(1,ispn,j),fvir)
+!        zt2=zzero
+!        istsv=ist+(ispn-1)*nstfv
+!        do i=1,nstsv
+!          zt2=zt2+dconjg(wanncnrloc(n,i,ikloc)*evecsvnr(istsv,i))
+!        enddo !i
+!        write(220,'("    n : ",I4,"  ispn : ",I4,&
+!          &"  numerical : ",2G18.10,"  analytical : ",2G18.10,&
+!          &"  diff : ",G18.10)')n,ispn,dreal(zt1),dimag(zt1),&
+!          dreal(zt2),dimag(zt2),abs(zt1-zt2)
+!      enddo
+!    enddo
+!  enddo !ist
+!  write(220,*)
+!  call flushifc(220)
 enddo !ikloc
-call mpi_grid_reduce(t1,dims=(/dim_k/),op=op_max)
-if (wproc) then
-  write(fout,*)
-  write(fout,'("Maximum deviation for <W_n|\phi_{jk}> : ",G18.10)')t1
-endif
-close(220)
+!write(220,*)
+
+!! == test2: W_{nk} is analytical, integration is analytical ==
+!write(220,'("test2")')
+!do ikloc=1,1!nkptnrloc
+!  ik=mpi_grid_map(nkptnr,dim_k,loc=ikloc)
+!  write(220,'("ik : ",I4,"  vklnr : ",3G18.10)')ik,vklnr(:,ik)
+!  call getevecfv(vklnr(1,ik),vgklnr(1,1,ikloc),evecfvnr)
+!  call getevecsv(vklnr(1,ik),evecsvnr)
+!  call match(ngknr(ikloc),gknr(1,ikloc),tpgknr(1,1,ikloc),&
+!    sfacgknr(1,1,ikloc),apwalm)
+!  call genwffvmt(lmaxvr,lmmaxvr,ngknr(ikloc),evecfvnr,apwalm,wffvmt)
+!
+!  do ist=1,nstfv
+!    write(220,'("  ist : ",I4)')ist
+!    fvir=zzero
+!    do ig=1,ngknr(ikloc)
+!      fvir(igfft(igkignr(ig,ikloc)))=evecfvnr(ig,ist,1)
+!    enddo
+!    call zfftifc(3,ngrid,1,fvir)
+!    do ir=1,ngrtot
+!      fvir(ir)=fvir(ir)*cfunir(ir)
+!    enddo
+!    call zfftifc(3,ngrid,-1,fvir)
+!    do j=1,sic_wantran%nwan
+!      n=sic_wantran%iwan(j)
+!      do ispn=1,nspinor
+!        zt1=zzero
+!        do ias=1,natmtot
+!          do lm=1,lmmaxvr
+!            do io1=1,nufr(lm2l(lm),ias2is(ias))
+!              do io2=1,nufr(lm2l(lm),ias2is(ias))
+!                zt1=zt1+dconjg(wann_unkmt(lm,io1,ias,ispn,n,ikloc))*&
+!                  wffvmt(lm,io2,ias,ist)*ufrp(lm2l(lm),io1,io2,ias2ic(ias))
+!              enddo
+!            enddo
+!          enddo !lm
+!        enddo
+!        do ig=1,ngknr(ikloc)
+!          zt1=zt1+dconjg(wann_unkit(ig,ispn,n,ikloc))*&
+!            fvir(igfft(igkignr(ig,ikloc)))
+!        enddo
+!        zt2=zzero
+!        istsv=ist+(ispn-1)*nstfv
+!        do i=1,nstsv
+!          zt2=zt2+dconjg(wanncnrloc(n,i,ikloc)*evecsvnr(istsv,i))
+!        enddo !i
+!        write(220,'("    n : ",I4,"  ispn : ",I4,&
+!          &"  numerical : ",2G18.10,"  analytical : ",2G18.10,&
+!          &"  diff : ",G18.10)')n,ispn,dreal(zt1),dimag(zt1),&
+!          dreal(zt2),dimag(zt2),abs(zt1-zt2)
+!      enddo
+!    enddo
+!  enddo !ist
+!  write(220,*)
+!  call flushifc(220)
+!enddo !ikloc
+
+
+!t1=0.d0
+!
+!
+!
+!
+!do ikloc=1,1 !nkptnrloc
+!  ik=mpi_grid_map(nkptnr,dim_k,loc=ikloc)
+!  write(220,'("ik : ",I4,"  vklnr : ",3G18.10)')ik,vklnr(:,ik)
+!  call getevecfv(vklnr(1,ik),vgklnr(1,1,ikloc),evecfvnr)
+!  call getevecsv(vklnr(1,ik),evecsvnr)
+!  call match(ngknr(ikloc),gknr(1,ikloc),tpgknr(1,1,ikloc),&
+!    sfacgknr(1,1,ikloc),apwalm)
+!  call genwffvmt(lmaxvr,lmmaxvr,ngknr(ikloc),evecfvnr,apwalm,wffvmt)
+!  do ist=1,nstfv
+!    write(220,'("  ist : ",I4)')ist
+!    do ir=1,s_nr
+!      do itp=1,s_ntp
+!        vrc(:)=s_spx(:,itp)*s_r(ir)
+!        call s_get_wffvval(vrc,ngknr(ikloc),vkcnr(1,ik),vgkcnr(1,1,ikloc), &
+!          wffvmt(1,1,1,ist),evecfvnr(1,ist,1),wffvtp(itp,ir))
+!      enddo
+!    enddo
+!    do ir=1,3000
+!      vrc(:)=(/ir*10.d0/3000.d0,0.d0,0.d0/)
+!      call s_get_wffvval(vrc,ngknr(ikloc),vkcnr(1,ik),vgkcnr(1,1,ikloc), &
+!          wffvmt(1,1,1,ist),evecfvnr(1,ist,1),zt1)
+!      write(200,*)vrc(1),dreal(zt1),dimag(zt1)
+!    enddo
+!    write(200,*)
+!!    call zgemm('T','N',lmmaxwan,s_nr,s_ntp,zone,s_ylmb,s_ntp,wffvtp,&
+!!      s_ntp,zzero,wffvlm,lmmaxwan)
+!    do j=1,sic_wantran%nwan
+!      n=sic_wantran%iwan(j)
+!      do ispn=1,nspinor
+!        call zgemm('T','N',s_ntp,s_nr,lmmaxwan,zone,s_ylmf,lmmaxwan,s_wanlm(1,1,ispn,j),&
+!          lmmaxwan,zzero,wantp,s_ntp)
+!
+!        zt1=zzero
+!        !do ir=1,s_nr
+!        !  zt1=zt1+zdotc(lmmaxwan,s_wanlm(1,ir,ispn,j),1,wffvlm(1,ir),1)*s_rw(ir)
+!        !enddo
+!        do ir=1,s_nr
+!          do itp=1,s_ntp
+!            zt1=zt1+dconjg(wantp(itp,ir))*wffvtp(itp,ir)*s_tpw(itp)*s_rw(ir)
+!          enddo
+!        enddo
+!        zt2=zzero
+!        istsv=ist+(ispn-1)*nstfv
+!        do i=1,nstsv
+!          zt2=zt2+dconjg(wanncnrloc(n,i,ikloc)*evecsvnr(istsv,i))
+!        enddo !i
+!        t1=max(t1,abs(zt1-zt2))
+!        write(220,'("    n : ",I4,"  ispn : ",I4,&
+!          &"  numerical : ",2G18.10,"  analytical : ",2G18.10,&
+!          &"  diff : ",G18.10)')n,ispn,dreal(zt1),dimag(zt1),&
+!          dreal(zt2),dimag(zt2),abs(zt1-zt2)
+!      enddo !ispn
+!    enddo !j
+!  enddo !ist
+!  write(220,*)
+!  call flushifc(220)
+!enddo !ikloc
+!call mpi_grid_reduce(t1,dims=(/dim_k/),op=op_max)
+!if (wproc) then
+!  write(fout,*)
+!  write(fout,'("Maximum deviation for <W_n|\phi_{jk}> : ",G18.10)')t1
+!endif
+!close(220)
+!call bstop
 deallocate(apwalm)
-deallocate(wffvmt)
+!deallocate(wffvmt)
 deallocate(evecfvnr)
 deallocate(evecsvnr)
-deallocate(wffvtp)
-deallocate(wffvlm)
+!deallocate(wffvtp)
+!deallocate(wffvlm)
+!deallocate(wantp)
+deallocate(wnkmt,wnkir)
+deallocate(wfmt)
 return
 end
 
