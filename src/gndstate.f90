5733270691f9fc88876a9c241eb601c37e203319
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-03-14 10:04:27 +0000

    charge density and magnetization moved to a separate file

--- src/gndstate.f90
+++ src/gndstate.f90
@@ -201,54 +201,7 @@ do iscl=1,maxscl
   call mpi_grid_bcast(occsv(1,1),nstsv*nkpt,dims=(/dim_k,dim2/))
   if (wannier) call wann_ene_occ  
   if (sic) call sic_e0
-! set the charge density and magnetisation to zero
-  rhomt(:,:,:)=0.d0
-  rhoir(:)=0.d0
-  if (spinpol) then
-    magmt(:,:,:,:)=0.d0
-    magir(:,:)=0.d0
-  end if
-  do ikloc=1,nkptloc
-    call rhomagk(ikloc,evecfvloc(1,1,1,ikloc),evecsvloc(1,1,ikloc))
-  end do
-  call mpi_grid_reduce(rhomt(1,1,1),lmmaxvr*nrmtmax*natmtot,&
-    dims=(/dim_k,dim2/))
-  call mpi_grid_reduce(rhoir(1),ngrtot,dims=(/dim_k,dim2/))
-  if (spinpol) then
-    call mpi_grid_reduce(magmt(1,1,1,1),lmmaxvr*nrmtmax*natmtot*ndmag,&
-      dims=(/dim_k,dim2/))
-    call mpi_grid_reduce(magir(1,1),ngrtot*ndmag,dims=(/dim_k,dim2/))
-  endif
-  if (mpi_grid_root(dims=(/dim_k,dim2/))) then
-! convert muffin-tin density/magnetisation to spherical harmonics
-    call rhomagsh
-! symmetrise the density
-    call symrf(lradstp,rhomt,rhoir)
-! symmetrise the magnetisation
-    if (spinpol) call symrvf(lradstp,magmt,magir)
-! convert the density from a coarse to a fine radial mesh
-    call rfmtctof(rhomt)
-! convert the magnetisation from a coarse to a fine radial mesh
-    do idm=1,ndmag
-      call rfmtctof(magmt(:,:,:,idm))
-    end do
-! add the core density to the total density
-    call addrhocr
-  endif
-  call mpi_grid_bcast(rhomt(1,1,1),lmmaxvr*nrmtmax*natmtot,&
-    dims=(/dim_k,dim2/))
-  call mpi_grid_bcast(rhoir(1),ngrtot,dims=(/dim_k,dim2/))
-  if (spinpol) then
-    call mpi_grid_bcast(magmt(1,1,1,1),lmmaxvr*nrmtmax*natmtot*ndmag,&
-      dims=(/dim_k,dim2/))
-    call mpi_grid_bcast(magir(1,1),ngrtot*ndmag,dims=(/dim_k,dim2/))
-  endif
-! calculate the charges
-  call charge
-! calculate the moments
-  if (spinpol) call moment
-! normalise the density
-  call rhonorm
+  call rhomag
 ! LDA+U
   if (ldapu.ne.0) then
 ! generate the LDA+U density matrix
