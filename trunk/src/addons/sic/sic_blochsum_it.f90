990b1583bbc4d92e95993c667ce673530b0bd5bd
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-03-16 23:34:04 +0000

    speedup modifications

--- trunk/src/addons/sic/sic_blochsum_it.f90
+++ trunk/src/addons/sic/sic_blochsum_it.f90
@@ -4,14 +4,12 @@ use mod_sic
 use mod_nrkp
 implicit none
 integer ikloc,ik,j,igg,ig,ig1,ir,i,ispn,l,lm,n
-integer vgl(3),glim(2,3),gglim(2,3),ngg
+integer vgl(3)
 real(8) vg(3),t1,tp(2)
 complex(8) zt1,zt2
-integer, allocatable :: ggdone(:)
-integer, allocatable :: ggidx(:,:,:)
 real(8), allocatable :: jl(:,:)
-complex(8), allocatable :: wggk(:,:,:)
-complex(8), allocatable :: wvggk(:,:,:)
+complex(8), allocatable :: wgk(:,:,:)
+complex(8), allocatable :: wvgk(:,:,:)
 complex(8), allocatable :: zf(:)
 complex(8), allocatable :: ylmgk(:)
 !
@@ -22,107 +20,70 @@ call timer_start(90,reset=.true.)
 allocate(jl(s_nr,0:lmaxwan))
 allocate(zf(s_nr))
 allocate(ylmgk(lmmaxwan))
+allocate(wgk(ngkmax,nspinor,sic_wantran%nwan))
+allocate(wvgk(ngkmax,nspinor,sic_wantran%nwan))
 do ikloc=1,nkptloc
   ik=mpi_grid_map(nkpt,dim_k,loc=ikloc)
-  glim=0
+  wgk=zzero
+  wvgk=zzero
   do ig=1,ngk(1,ik)
-    do i=1,3
-      glim(1,i)=min(glim(1,i),ivg(i,igkig(ig,1,ikloc)))
-      glim(2,i)=max(glim(2,i),ivg(i,igkig(ig,1,ikloc)))
-    enddo
-  enddo
-  do i=1,3
-    gglim(1,i)=glim(1,i)+intgv(i,1)
-    gglim(2,i)=glim(2,i)+intgv(i,2)
-  enddo
-  allocate(ggidx(gglim(1,1):gglim(2,1),gglim(1,2):gglim(2,2),gglim(1,3):gglim(2,3)))
-  ngg=0
-  ggidx=0
-  do ig=1,ngk(1,ik)
-    do ig1=1,ngrtot
-      vgl(:)=ivg(:,igkig(ig,1,ikloc))+ivg(:,ig1)
-      if (ggidx(vgl(1),vgl(2),vgl(3)).eq.0) then
-        ngg=ngg+1
-        ggidx(vgl(1),vgl(2),vgl(3))=ngg
-      endif
-    enddo
-  enddo
-  allocate(wggk(ngg,nspinor,sic_wantran%nwan))
-  allocate(wvggk(ngg,nspinor,sic_wantran%nwan))
-  allocate(ggdone(ngg))
-  wggk=zzero
-  wvggk=zzero
-  ggdone=0
-  do ig=1,ngk(1,ik)
-    do ig1=1,ngrtot
-      vgl(:)=ivg(:,igkig(ig,1,ikloc))+ivg(:,ig1)
-      igg=ggidx(vgl(1),vgl(2),vgl(3))
-      if (igg.ne.0.and.ggdone(igg).eq.0) then
-        ggdone(igg)=1
-        vg(:)=vgkc(:,ig,1,ikloc)+vgc(:,ig1)
-        call sphcrd(vg,t1,tp)
+    call sphcrd(vgkc(1,ig,1,ikloc),t1,tp)
 ! generate Bessel functions j_l(|G+G'+k|x)
-        do ir=1,s_nr
-          call sbessel(lmaxwan,t1*s_r(ir),jl(ir,:))
-        enddo
-        call genylm(lmaxwan,tp,ylmgk)
-        do j=1,sic_wantran%nwan
-          n=sic_wantran%iwan(j)
-          do ispn=1,nspinor
-! compute <G+G'+k|W_n>
-            zt1=zzero
-            do l=0,lmaxwan
-              zf=zzero
-              do ir=1,s_nr
-                do lm=l**2+1,(l+1)**2
-                  zf(ir)=zf(ir)+s_wanlm(lm,ir,ispn,j)*ylmgk(lm)
-                enddo
-              enddo
-              zt2=zzero
-              do ir=1,s_nr
-                zt2=zt2+jl(ir,l)*zf(ir)*s_rw(ir)
-              enddo
-              zt1=zt1+zt2*fourpi*((-zi)**l)/sqrt(omega)
-            enddo !l
-            wggk(igg,ispn,j)=zt1*exp(-zi*dot_product(wanpos(:,n),vg(:)))
-! compute <G+G'+k|(WV)_n>
-            zt1=zzero
-            do l=0,lmaxwan
-              zf=zzero
-              do ir=1,s_nr
-                do lm=l**2+1,(l+1)**2
-                  zf(ir)=zf(ir)+s_wvlm(lm,ir,ispn,j)*ylmgk(lm)
-                enddo
-              enddo
-              zt2=zzero
-              do ir=1,s_nr
-                zt2=zt2+jl(ir,l)*zf(ir)*s_rw(ir)
-              enddo
-              zt1=zt1+zt2*fourpi*((-zi)**l)/sqrt(omega)
-            enddo !l
-            wvggk(igg,ispn,j)=zt1*exp(-zi*dot_product(wanpos(:,n),vg(:)))
-           enddo !ispn
-        enddo !j
-      endif
-    enddo !ig1
+    do ir=1,s_nr
+      call sbessel(lmaxwan,t1*s_r(ir),jl(ir,:))
+    enddo
+    call genylm(lmaxwan,tp,ylmgk)
+    do j=1,sic_wantran%nwan
+      n=sic_wantran%iwan(j)
+      do ispn=1,nspinor
+! compute <G+k|W_n>
+        zt1=zzero
+        do l=0,lmaxwan
+          zf=zzero
+          do ir=1,s_nr
+            do lm=l**2+1,(l+1)**2
+              zf(ir)=zf(ir)+s_wanlm(lm,ir,ispn,j)*ylmgk(lm)
+            enddo
+          enddo
+          zt2=zzero
+          do ir=1,s_nr
+            zt2=zt2+jl(ir,l)*zf(ir)*s_rw(ir)
+          enddo
+          zt1=zt1+zt2*fourpi*((-zi)**l)/sqrt(omega)
+        enddo !l
+        wgk(ig,ispn,j)=zt1*exp(-zi*dot_product(wanpos(:,n),vgkc(:,ig,1,ikloc)))
+! compute <G+k|(WV)_n>
+        zt1=zzero
+        do l=0,lmaxwan
+          zf=zzero
+          do ir=1,s_nr
+            do lm=l**2+1,(l+1)**2
+              zf(ir)=zf(ir)+s_wvlm(lm,ir,ispn,j)*ylmgk(lm)
+            enddo
+          enddo
+          zt2=zzero
+          do ir=1,s_nr
+            zt2=zt2+jl(ir,l)*zf(ir)*s_rw(ir)
+          enddo
+          zt1=zt1+zt2*fourpi*((-zi)**l)/sqrt(omega)
+        enddo !l
+        wvgk(ig,ispn,j)=zt1*exp(-zi*dot_product(wanpos(:,n),vgkc(:,ig,1,ikloc)))
+      enddo !ispn
+    enddo !j
   enddo !ig
   do ig=1,ngk(1,ik)
-    do ig1=1,ngrtot
-      vgl(:)=ivg(:,igkig(ig,1,ikloc))+ivg(:,ig1)
-      igg=ggidx(vgl(1),vgl(2),vgl(3))
-      if (igg.ne.0) then
-        s_wankir(ig,:,:,ikloc)=s_wankir(ig,:,:,ikloc)+&
-          wggk(igg,:,:)*cfunig(ig1)
-        s_wvkir(ig,:,:,ikloc)=s_wvkir(ig,:,:,ikloc)+&
-          wvggk(igg,:,:)*cfunig(ig1)
-      endif
+    do ig1=1,ngk(1,ik)
+      vgl(:)=ivg(:,igkig(ig,1,ikloc))-ivg(:,igkig(ig1,1,ikloc))
+      igg=ivgig(vgl(1),vgl(2),vgl(3))
+      s_wankir(ig,:,:,ikloc)=s_wankir(ig,:,:,ikloc)+&
+        wgk(ig1,:,:)*cfunig(igg)
+      s_wvkir(ig,:,:,ikloc)=s_wvkir(ig,:,:,ikloc)+&
+        wvgk(ig1,:,:)*cfunig(igg)
     enddo
   enddo
-  deallocate(ggidx)
-  deallocate(ggdone)
-  deallocate(wggk)
-  deallocate(wvggk)
 enddo !ikloc
+deallocate(wgk)
+deallocate(wvgk)
 deallocate(jl)
 deallocate(zf)
 deallocate(ylmgk)
