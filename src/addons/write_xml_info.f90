3f79bd0bc02b57dd35d20313d0e7b2d2a6e210bc
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-03-21 12:35:49 +0000

    WFs magnetic moment

--- src/addons/write_xml_info.f90
+++ src/addons/write_xml_info.f90
@@ -3,13 +3,20 @@ use modmain
 use mod_wannier
 implicit none
 integer ias,n
+real(8) momwf
+
 if (mpi_grid_root()) then
   open(300,file="info.xml",form="formatted",status="replace")
   write(300,'("<?xml version=""1.0""?>")')
   write(300,'("<info>")')
   do ias=1,natmtot
+    momwf=0.d0
+    do n=1,nwantot
+      if (wan_info(1,n).eq.ias) momwf=momwf+wanmom(n)
+    enddo
     write(300,'("  <atom id=""",I6,""">")')ias
-    write(300,'("    <magmom units=""a.u."">",F18.10,"</magmom>")')mommt(1,ias)
+    write(300,'("    <magmom_mt units=""a.u."">",F18.10,"</magmom_mt>")')mommt(1,ias)
+    write(300,'("    <magmom_wf units=""a.u."">",F18.10,"</magmom_wf>")')momwf
     write(300,'("  </atom>")')
   enddo
   do n=1,nwantot
