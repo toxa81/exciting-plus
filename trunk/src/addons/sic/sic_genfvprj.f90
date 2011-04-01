a3b909dc80cda344ccd0c99ca798a0480a459adc
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-04-01 10:03:06 +0000

    debug output in sic_genfvprj.f90

--- trunk/src/addons/sic/sic_genfvprj.f90
+++ trunk/src/addons/sic/sic_genfvprj.f90
@@ -8,14 +8,21 @@ complex(8), intent(in) :: evecfv(nmatmax,nstfv,nspnfv)
 complex(8), intent(in) :: apwalm(ngkmax,apwordmax,lmmaxapw,natmtot)
 ! local variables
 integer ik,ispn
-integer ias,ist,j
+integer ias,ist,j,j1
 complex(8) zt2(2)
 complex(8), allocatable :: wfmt_(:,:)
 complex(8), allocatable :: wfmt(:,:,:)
+complex(8), allocatable :: wb(:,:,:,:)
+complex(8), allocatable :: om(:,:)
 !
 sic_wb(:,:,:,ikloc)=zzero
 sic_wvb(:,:,:,ikloc)=zzero
 if (.not.tsic_wv) return 
+if (debug_level.ge.4) then
+  allocate(wb(3,sic_wantran%nwan,nstfv,nspinor))
+  wb=zzero
+  allocate(om(sic_wantran%nwan,sic_wantran%nwan))
+endif
 call timer_start(t_sic_genfvprj)
 !do ikloc=1,nkptloc
 !  do j=1,sic_wantran%nwan
@@ -48,23 +55,58 @@ do ist=1,nstfv
   enddo
   do j=1,sic_wantran%nwan
     do ispn=1,nspinor
+      zt2=zzero
       sic_wb(j,ist,ispn,ikloc)=s_zfinp(.false.,.true.,mt_ntp,ngk(1,ik),&
         s_wankmt(1,1,1,ispn,j,ikloc),wfmt,s_wankir(1,ispn,j,ikloc),&
-        evecfv(1,ist,1))
+        evecfv(1,ist,1),zt2)
+      if (debug_level.ge.4) then  
+        wb(1,j,ist,ispn)=sic_wb(j,ist,ispn,ikloc)
+        wb(2:3,j,ist,ispn)=zt2(:)
+      endif
       sic_wvb(j,ist,ispn,ikloc)=s_zfinp(.false.,.true.,mt_ntp,ngk(1,ik),&
         s_wvkmt(1,1,1,ispn,j,ikloc),wfmt,s_wvkir(1,ispn,j,ikloc),&
         evecfv(1,ist,1))
     enddo
   enddo
-  !write(*,'("    i : ",I4,"  re,im,abs : ",3F10.6," ! mt,it : ",&
-  !        2F10.6,",",2F10.6)')ist,dreal(sic_wb(1,ist,1,ik)),dimag(sic_wb(1,ist,1,ik)),&
-  !        abs(sic_wb(1,ist,1,ik)),dreal(zt2(1)),dimag(zt2(1)),&
-  !        dreal(zt2(2)),dimag(zt2(2))
 enddo !ist
 !$OMP END DO
-!write(*,*)
 deallocate(wfmt_,wfmt)
 !$OMP END PARALLEL
 call timer_stop(t_sic_genfvprj)
+if (debug_level.ge.4) then
+  call dbg_open_file
+  write(fdbgout,*)
+  write(fdbgout,'("[sic_genfvprj] ik : ",I4,"    vkl : ",3F10.6)')ik,vkl(:,ik)
+  do ist=1,nstfv
+    write(fdbgout,'("  ist : ",I4)')ist
+    do j=1,sic_wantran%nwan 
+      write(fdbgout,'("    j  : ",I4)')j
+      do ispn=1,nspinor
+        write(fdbgout,'("     total : ",F12.8," (",2F12.8,")")')abs(wb(1,j,ist,ispn)),&
+          dreal(wb(1,j,ist,ispn)),dimag(wb(1,j,ist,ispn))
+        write(fdbgout,'("        mt : ",F12.8," (",2F12.8,")")')abs(wb(2,j,ist,ispn)),&
+          dreal(wb(2,j,ist,ispn)),dimag(wb(2,j,ist,ispn))
+        write(fdbgout,'("        it : ",F12.8," (",2F12.8,")")')abs(wb(3,j,ist,ispn)),&
+          dreal(wb(3,j,ist,ispn)),dimag(wb(3,j,ist,ispn))
+      enddo
+    enddo
+  enddo
+  om=zzero
+  do j=1,sic_wantran%nwan
+    do j1=1,sic_wantran%nwan
+      do ispn=1,nspinor
+        do ist=1,nstfv
+          om(j,j1)=om(j,j1)+wb(1,j,ist,ispn)*dconjg(wb(1,j1,ist,ispn))
+        enddo
+      enddo
+    enddo
+  enddo
+  write(fdbgout,'("  overlap matrix")')
+  do j=1,sic_wantran%nwan
+    write(fdbgout,'(255F12.6)')(abs(om(j,j1)),j1=1,sic_wantran%nwan)
+  enddo
+  call dbg_close_file
+  deallocate(wb,om)
+endif
 return
 end
