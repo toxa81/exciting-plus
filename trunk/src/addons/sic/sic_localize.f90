7ea0c8dc226f3651aab50374644a9f52b6d82d75
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-05-27 11:09:35 +0000

    sic_localize.f90 is updated

--- trunk/src/addons/sic/sic_localize.f90
+++ trunk/src/addons/sic/sic_localize.f90
@@ -8,12 +8,10 @@ real(8), intent(out) :: wanprop(nwanprop,sic_wantran%nwan)
 integer iter,i,j,n,n1,j1,n2,j2,ispn,ir,ikloc
 real(8) tot_diff
 complex(8), allocatable :: um(:,:),um1(:,:),um0(:,:)
-complex(8), allocatable :: wlm_new(:,:)
 !
 allocate(um(sic_wantran%nwan,sic_wantran%nwan))
 allocate(um0(sic_wantran%nwan,sic_wantran%nwan))
 allocate(um1(sic_wantran%nwan,sic_wantran%nwan))
-allocate(wlm_new(lmmaxwan,sic_wantran%nwan))
 if (wproc) then
   write(fout,*)
   write(fout,'(80("="))')
@@ -32,31 +30,27 @@ do iter=1,sic_niter_u0
     tot_diff=0.d0
     call sic_exp_grad_u((/0.d0,0.d0,0.d0/),sic_u0_eps,tot_diff,um)
     if (wproc) then
-      write(fout,'("  total localization error : ",G18.10)')tot_diff
-      !write(fout,'("    SIC potential energy : ",G18.10)')&
-      !  sum(0.5d0*wanprop(wp_vha,:))+sum(wanprop(wp_exc,:))
+      write(fout,*)
+      write(fout,'("  localization error : ",G18.10)')tot_diff
+      write(fout,'("    potential energy : ",G18.10)')&
+        sum(0.5d0*wanprop(wp_vha,:))+sum(wanprop(wp_exc,:))
     endif
-    um1=zzero
-    do j1=1,sic_wantran%nwan
-      do j2=1,sic_wantran%nwan
-        do j=1,sic_wantran%nwan
-          um1(j1,j2)=um1(j1,j2)+um0(j1,j)*um(j,j2)
-        enddo
+! update um0 matrix: um0 -> um0*um (total unitary transformation)
+    call zgemm('N','N',sic_wantran%nwan,sic_wantran%nwan,sic_wantran%nwan,&
+      zone,um0,sic_wantran%nwan,um,sic_wantran%nwan,zzero,um1,sic_wantran%nwan)
+    um0(:,:)=um1(:,:)
+! transform Wannier functions (s_wvlm is used as temporary array)
+    s_wvlm=zzero
+    do j=1,sic_wantran%nwan
+      do j1=1,sic_wantran%nwan
+        call zaxpy(lmmaxwan*s_nr*nspinor,um(j1,j),s_wlm(1,1,1,j1),1,&
+          s_wvlm(1,1,1,j),1)
       enddo
     enddo
-    um0(:,:)=um1(:,:)
-    do ispn=1,nspinor
-      do ir=1,s_nr
-        wlm_new=zzero
-        do j=1,sic_wantran%nwan 
-          do j1=1,sic_wantran%nwan
-            wlm_new(:,j)=wlm_new(:,j)+um(j1,j)*s_wlm(:,ir,ispn,j1)
-          enddo
-        enddo
-        s_wlm(:,ir,ispn,:)=wlm_new(:,:)
-      enddo
-    enddo    
+    s_wlm=s_wvlm
   endif
+! generate new potentials
+  s_wvlm=zzero
   do j=1,sic_wantran%nwan
     n=sic_wantran%iwan(j)  
     if (sic_apply(n).eq.2) then
@@ -66,7 +60,7 @@ do iter=1,sic_niter_u0
     endif
   enddo
 enddo !iter
-deallocate(um,um1,wlm_new)
+deallocate(um,um1)
 allocate(um(nwantot,nwantot),um1(nwantot,nwantot))
 um=zzero
 do i=1,nwantot
@@ -81,14 +75,8 @@ do j1=1,sic_wantran%nwan
 enddo
 ! update k-independent part of sic_wan_umtrx
 do ikloc=1,nkptnrloc
-  um1=zzero
-  do n1=1,nwantot
-    do n2=1,nwantot
-      do n=1,nwantot
-        um1(n1,n2)=um1(n1,n2)+sic_wan_umtrx(n1,n,ikloc)*um(n,n2)
-      enddo
-    enddo
-  enddo
+  call zgemm('N','N',nwantot,nwantot,nwantot,zone,sic_wan_umtrx(1,1,ikloc),&
+    nwantot,um,nwantot,zzero,um1,nwantot)
   sic_wan_umtrx(:,:,ikloc)=um1(:,:)
 enddo
 deallocate(um,um0,um1)
