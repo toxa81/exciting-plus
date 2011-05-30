4fea233f04f764efee314a7129f74bad6368be35
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-05-30 12:21:54 +0000

    reverted genveffig.f90

--- trunk/src/genveffig.f90
+++ trunk/src/genveffig.f90
@@ -24,23 +24,10 @@ integer ig,ifg
 ! allocatable arrays
 complex(8), allocatable :: zfft(:)
 allocate(zfft(ngrtot))
-! Fourier transform effective potential to G-space
-zfft(:)=veffir(:)
+! multiply effective potential with characteristic function
+zfft(:)=veffir(:)*cfunir(:)
+! Fourier transform to G-space
 call zfftifc(3,ngrid,-1,zfft)
-! zero Fourier components for |G| > 2*gkmax
-do ig=1,ngrtot
-  if (gc(ig).gt.2.d0*gkmax) then
-    ifg=igfft(ig)
-    zfft(ifg)=0.d0
-  end if
-end do
-! Fourier transform back to real space
-call zfftifc(3,ngrid,1,zfft)
-! multiply by characteristic function
-zfft(:)=dble(zfft(:))*cfunir(:)
-! Fourier transform back to G-space
-call zfftifc(3,ngrid,-1,zfft)
-! store in global array
 do ig=1,ngvec
   ifg=igfft(ig)
   veffig(ig)=zfft(ifg)
