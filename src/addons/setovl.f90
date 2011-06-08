fe848c59272b7d186c5e86dea24b7f89478dbe8a
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-06-08 14:44:13 +0000

    Warning! Test of optimized overlap matrix setup

--- src/addons/setovl.f90
+++ src/addons/setovl.f90
@@ -6,34 +6,54 @@ integer, intent(in) :: nmatp
 integer, intent(in) :: igpig(ngkmax)
 complex(8), intent(in) :: apwalm(ngkmax,apwordmax,lmmaxapw,natmtot)
 complex(8), intent(out) :: o(nmatp,nmatp)
+complex(8), allocatable :: zm1(:,:)
 
 complex(8) zt1
-integer is,ia,ias,ig
+integer is,ia,ias,ig,naa
 integer l1,m1,lm1,io1
 integer i,j,ilo1,ilo2
-integer natmtotloc,iasloc
 integer iv(3)
 
 call timer_start(t_seceqnfv_setup_o)
 call timer_start(t_seceqnfv_setup_o_mt)
-natmtotloc=mpi_grid_map(natmtot,dim2)
-do iasloc=1,natmtotloc
-  ias=mpi_grid_map(natmtot,dim2,loc=iasloc)
+allocate(zm1(apwordmax*lmmaxapw*natmtot,ngkmax))
+zm1=zzero
+naa=0
+do ias=1,natmtot
+  is=ias2is(ias)
+  ia=ias2ia(ias)
+  do l1=0,lmaxmat
+    do m1=-l1,l1
+      lm1=idxlm(l1,m1)
+      do io1=1,apword(l1,is)
+         naa=naa+1
+         zm1(naa,:)=apwalm(:,io1,lm1,ias)
+      enddo
+    enddo
+  enddo
+enddo
+!----------------------!
+!     APW-APW term     !
+!----------------------!
+call zgemm('C','N',ngp,ngp,naa,zone,zm1,apwordmax*lmmaxapw*natmtot,&
+  zm1,apwordmax*lmmaxapw*natmtot,zone,o(1,1),nmatp)
+deallocate(zm1)
+do ias=1,natmtot
   is=ias2is(ias)
   ia=ias2ia(ias)
 !----------------------!
 !     APW-APW term     !
 !----------------------!
-  do l1=0,lmaxmat
-    do m1=-l1,l1
-      lm1=idxlm(l1,m1)
-      do io1=1,apword(l1,is)
-        do ig=1,ngp
-          call zaxpy(ig,dconjg(apwalm(ig,io1,lm1,ias)),apwalm(:,io1,lm1,ias),1,o(:,ig),1)
-        enddo
-      enddo !io1
-    enddo !m1
-  enddo !l1
+!  do l1=0,lmaxmat
+!    do m1=-l1,l1
+!      lm1=idxlm(l1,m1)
+!      do io1=1,apword(l1,is)
+!        do ig=1,ngp
+!          call zaxpy(ig,dconjg(apwalm(ig,io1,lm1,ias)),apwalm(:,io1,lm1,ias),1,o(:,ig),1)
+!        enddo
+!      enddo !io1
+!    enddo !m1
+!  enddo !l1
 !---------------------!
 !     APW-lo term     !
 !---------------------!  
@@ -43,8 +63,10 @@ do iasloc=1,natmtotloc
       lm1=idxlm(l1,m1)
       i=ngp+idxlo(lm1,ilo1,ias)
       do io1=1,apword(l1,is)
-        zt1=dcmplx(oalo(io1,ilo1,ias),0.d0)
-        call zaxpy(ngp,zt1,apwalm(:,io1,lm1,ias),1,o(:,i),1)
+        zt1=zone*oalo(io1,ilo1,ias)
+        do ig=1,ngp
+          o(ig,i)=o(ig,i)+dconjg(apwalm(ig,io1,lm1,ias))*oalo(io1,ilo1,ias)
+        end do
       end do
     end do
   end do
@@ -67,7 +89,6 @@ do iasloc=1,natmtotloc
     end do
   end do
 enddo
-call mpi_grid_reduce(o(1,1),nmatp*nmatp,dims=(/dim2/))
 call timer_stop(t_seceqnfv_setup_o_mt)
 call timer_start(t_seceqnfv_setup_o_it)
 !---------------------!
@@ -77,11 +98,10 @@ do j=1,ngp
   do i=1,j
     iv(:)=ivg(:,igpig(i))-ivg(:,igpig(j))
     ig=ivgig(iv(1),iv(2),iv(3))
-    o(i,j)=o(i,j)+dconjg(cfunig(ig))
+    o(i,j)=o(i,j)+cfunig(ig)
   end do
 end do
 call timer_stop(t_seceqnfv_setup_o_it)
-o=dconjg(o)
 call timer_stop(t_seceqnfv_setup_o)
 return
 end
