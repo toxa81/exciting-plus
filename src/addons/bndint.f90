20dff522c1ec37b4ec59148c48ec6ecd1f2392ec
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-06-09 21:58:38 +0000

    Warning! Test of new bndint.f90: now band inteval can be mixed N1/E1 to N2/E2

--- src/addons/bndint.f90
+++ src/addons/bndint.f90
@@ -7,23 +7,52 @@ real(8), intent(in) :: e
 real(8), intent(in) :: e1
 real(8), intent(in) :: e2
 
-logical leint
+logical leint1,leint2,l1,l2
 integer n1,n2
 
-leint=.true.
-n1=int(e1)
-n2=int(e2)
-if (abs(n1-e1).lt.1d-5.and.abs(n2-e2).lt.1d-5) then
-  leint=.false.
+leint1=.true.
+leint2=.true.
+if (e1.gt.0.d0) then
+  n1=int(e1)
+  if (abs(dble(n1)-e1).lt.1d-5) leint1=.false.
 endif
-if (leint) then
-  bndint=(e.ge.e1.and.e.le.e2)
+if (e2.gt.0.d0) then
+  n2=int(e2)
+  if (abs(dble(n2)-e2).lt.1d-5) leint2=.false.
+endif
+
+if (leint1) then
+  l1=(e.ge.e1)
 else
-  if (ncmag.or.ldisentangle) then
-    bndint=(j.ge.n1.and.j.le.n2)
+ if (ncmag.or.ldisentangle) then
+    l1=(j.ge.n1)
   else
-    bndint=((mod(j-1,nstfv)+1).ge.n1.and.(mod(j-1,nstfv)+1).le.n2)
+    l1=((mod(j-1,nstfv)+1).ge.n1)
   endif
 endif
+if (leint2) then
+  l2=(e.le.e2)
+else
+ if (ncmag.or.ldisentangle) then
+    l2=(j.le.n2)
+  else
+    l2=((mod(j-1,nstfv)+1).le.n2)
+  endif
+endif
+bndint=(l1.and.l2)
+!leint=.true.
+!if (abs(n1-e1).lt.1d-5.and.abs(n2-e2).lt.1d-5) then
+!  leint=.false.
+!endif
+!if (leint) then
+!  bndint=(e.ge.e1.and.e.le.e2)
+!else
+!  if (ncmag.or.ldisentangle) then
+!    bndint=(j.ge.n1.and.j.le.n2)
+!  else
+!    bndint=((mod(j-1,nstfv)+1).ge.n1.and.(mod(j-1,nstfv)+1).le.n2)
+!  endif
+!endif
 return
-end
+end function
+
