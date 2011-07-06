2d215dd5479d8e8232f0cdd775d0cb1c82476874
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-07-06 13:23:47 +0000

    optimized version of addons/rhomagk_exact.f90 with OMP support

--- src/addons/rhomagk_exact.f90
+++ src/addons/rhomagk_exact.f90
@@ -10,7 +10,7 @@ complex(8), intent(in) :: evecsv(nstsv,nstsv)
 integer ist,j,ilo,ia,io,l,lm,m
 integer is,ias,ik,ispn,jst,n,i1
 integer ivg3(3),ifg3
-integer ig1,ig2,ic,io1,io2,l1,l2,j1,j2,lm1,lm2,lm3,l3
+integer ig1,ig2,io1,io2,l1,l2,j1,j2,lm1,lm2,lm3,l3
 real(8) t1
 complex(8) zt1,zt2(nspinor)
 ! allocatable arrays
@@ -18,8 +18,10 @@ complex(8), allocatable :: apwalm(:,:,:,:)
 
 complex(8), allocatable :: wfsvmt(:,:,:,:,:)
 complex(8), allocatable :: wfsvit(:,:,:)
-complex(8), allocatable :: rhoitk(:,:)
+complex(8), allocatable :: wfsvit_(:,:,:)
 complex(8), allocatable :: gntmp(:,:)
+complex(8), allocatable :: zdens(:,:,:,:,:,:)
+complex(8) zv1(lmmaxapw,nufrmax)
 real(8), allocatable :: wo(:)
 integer, allocatable :: istocc(:)
 complex(8), allocatable :: evecfd_(:,:)
@@ -41,7 +43,6 @@ allocate(wfsvmt(lmmaxapw,nufrmax,natmtot,nspinor,nstocc))
 allocate(wfsvit(ngkmax,nspinor,nstocc))
 wfsvmt=zzero
 wfsvit=zzero
-! generate wave functions in muffin-tins
 if (tsveqn) then
   allocate(apwalm(ngkmax,apwordmax,lmmaxapw,natmtot))
 ! find the matching coefficients
@@ -62,12 +63,40 @@ else
 endif
 deallocate(apwalm)
 call timer_start(t_rho_mag_mt)
-allocate(gntmp(lmmaxapw,lmmaxapw))
+allocate(zdens(lmmaxapw,lmmaxapw,nufrmax,nufrmax,natmtot,nspinor))
+zdens=zzero
+!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(is,l1,io1,l2,io2,jst,ispn,zv1,lm1,lm2) 
+do ias=1,natmtot
+  is=ias2is(ias)
+  do l1=0,lmaxapw
+    do io1=1,nufr(l1,is)
+      do l2=0,lmaxapw
+        do io2=1,nufr(l2,is)
+          do jst=1,nstocc
+            do ispn=1,nspinor
+              zv1(:,:)=wfsvmt(:,:,ias,ispn,jst)
+              do lm2=l2**2+1,(l2+1)**2
+                do lm1=l1**2+1,(l1+1)**2
+                  zdens(lm1,lm2,io1,io2,ias,ispn)=zdens(lm1,lm2,io1,io2,ias,ispn)+&
+                    dconjg(zv1(lm1,io1))*zv1(lm2,io2)*wo(jst)
+                enddo
+              enddo
+            enddo
+          enddo !jst
+        enddo
+      enddo
+    enddo
+  enddo
+enddo
+!$OMP END PARALLEL DO
+!$OMP PARALLEL DEFAULT(SHARED) &
+!$OMP PRIVATE(gntmp,l1,l2,l3,ias,is,j1,j2,io1,io2,zt2,ispn,zt1)
+allocate(gntmp(lmmaxapw,lmmaxapw)) 
+!$OMP DO
 do lm3=1,lmmaxvr
   gntmp(:,:)=gntyry(lm3,:,:)
   l3=lm2l(lm3)
   do ias=1,natmtot
-    ic=ias2ic(ias)
     is=ias2is(ias)
     j1=0
     do l1=0,lmaxapw
@@ -79,43 +108,48 @@ do lm3=1,lmmaxvr
             j2=j2+1
             if (mod(l1+l2+l3,2).eq.0) then
               zt2=zzero
-              do jst=1,nstocc
-                do ispn=1,nspinor
+              do ispn=1,nspinor
+                zt1=zzero
+                do lm2=l2**2+1,(l2+1)**2
                   do lm1=l1**2+1,(l1+1)**2
-                    zt1=zzero
-                    do lm2=l2**2+1,(l2+1)**2
-                      zt1=zt1+wfsvmt(lm2,io2,ias,ispn,jst)*gntmp(lm2,lm1)
-                    enddo
-                    zt2(ispn)=zt2(ispn)+dconjg(wfsvmt(lm1,io1,ias,ispn,jst))*zt1*wo(jst)
+                    zt1=zt1+zdens(lm1,lm2,io1,io2,ias,ispn)*gntmp(lm1,lm2)
                   enddo
                 enddo
-              enddo !jst
+                zt2(ispn)=zt1
+              enddo
               rhomagmt(j1,j2,lm3,ias,:)=rhomagmt(j1,j2,lm3,ias,:)+dreal(zt2(:))
             endif
           enddo
         enddo
       enddo
     enddo
-  enddo !lm3
-enddo !ias
+  enddo !ias
+enddo !lm3
+!$OMP END DO
 deallocate(gntmp)
+!$OMP END PARALLEL 
 deallocate(wfsvmt)
+deallocate(zdens)
 call timer_stop(t_rho_mag_mt)
 call timer_start(t_rho_mag_it)
+allocate(wfsvit_(nstocc,ngkmax,nspinor))
+do jst=1,nstocc
+  wfsvit_(jst,:,:)=wfsvit(:,:,jst)
+enddo
 do ig1=1,ngk(1,ik)
   do ig2=1,ngk(1,ik)
     ivg3(:)=ivg(:,igkig(ig2,1,ikloc))-ivg(:,igkig(ig1,1,ikloc))
     ifg3=igfft(ivgig(ivg3(1),ivg3(2),ivg3(3)))
-    do jst=1,nstocc
-      do ispn=1,nspinor
+    do ispn=1,nspinor
+      do jst=1,nstocc
         rhomagit(ifg3,ispn)=rhomagit(ifg3,ispn)+&
-          dconjg(wfsvit(ig1,ispn,jst))*wfsvit(ig2,ispn,jst)*wo(jst)/omega
+          dconjg(wfsvit_(jst,ig1,ispn))*wfsvit_(jst,ig2,ispn)*wo(jst)/omega
       enddo
     enddo
   enddo
 enddo
 call timer_stop(t_rho_mag_it)
-deallocate(wfsvit)
+deallocate(wfsvit,wfsvit_)
 deallocate(wo,istocc)
 return
 end subroutine
