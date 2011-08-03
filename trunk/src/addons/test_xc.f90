a20496ecdc8208d8d9be58614cdd1e772c2845f3
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-08-03 14:10:42 +0000

    print xc up to density=2

--- trunk/src/addons/test_xc.f90
+++ trunk/src/addons/test_xc.f90
@@ -5,9 +5,9 @@ real(8) dens(nspinor),vx(nspinor),vc(nspinor),ex,ec
 integer i
 call init0
 
-do i=0,10000
+do i=0,20000
   dens=0.d0
-  dens(1)=dble(i)*0.01
+  dens(1)=dble(i)*0.0001
   call elk_xc(dens,vx,vc,ex,ec)
   write(100,*)dens(1),vx(1),ex
   write(200,*)dens(1),vc(1),ec
