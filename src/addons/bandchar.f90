2f8a0652f7f74f616d1c7d2c625393d47a080add
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-09-12 11:43:18 +0000

    modification in bandchar arguments

--- src/addons/bandchar.f90
+++ src/addons/bandchar.f90
@@ -1,29 +1,20 @@
-subroutine bandchar(tsym,ikloc,lmax,lmmax,evecfv,evecsv,bndchr)
+subroutine bandchar(tsym,lmax,lmmax,wfsvmt,bndchr)
 use modmain
 implicit none
 ! arguments
 logical, intent(in) :: tsym
-integer, intent(in) :: ikloc
 integer, intent(in) :: lmax
 integer, intent(in) :: lmmax
-complex(8), intent(in) :: evecfv(nmatmax,nstfv,nspnfv)
-complex(8), intent(in) :: evecsv(nstsv,nstsv)
+complex(8), intent(in) :: wfsvmt(lmmax,nufrmax,natmtot,nspinor,nstsv)
 real(4), intent(out) :: bndchr(lmmax,natmtot,nspinor,nstsv)
 ! local variables
 integer io1,io2,ispn1,ispn2,lm1,lm2,m1,m2
 integer l,lm,j,ik,ispn,ias,is,ic
 real(8) d1
 complex(8) z1
-complex(8), allocatable :: wfsvmt(:,:,:,:,:)
-complex(8), allocatable :: apwalm(:,:,:,:)
 complex(8), allocatable :: dmatylm(:,:,:,:,:)
 !
-ik=mpi_grid_map(nkpt,dim_k,loc=ikloc)
-allocate(wfsvmt(lmmax,nufrmax,natmtot,nspinor,nstsv))
-allocate(apwalm(ngkmax,apwordmax,lmmaxapw,natmtot))
 allocate(dmatylm(lmmax,lmmax,nspinor,nspinor,natmtot))
-call match(ngk(1,ik),gkc(1,1,ikloc),tpgkc(1,1,1,ikloc),sfacgk(1,1,1,ikloc),apwalm)
-call genwfsvmt(lmax,lmmax,ngk(1,ik),evecfv(:,:,1),evecsv,apwalm,wfsvmt)
 do j=1,nstsv
   dmatylm=zzero  
   do ias=1,natmtot
@@ -58,7 +49,7 @@ do j=1,nstsv
     enddo !ispn
   enddo !ias
 enddo !j
-deallocate(wfsvmt,apwalm,dmatylm)
+deallocate(dmatylm)
 return
 end subroutine
 !EOC
