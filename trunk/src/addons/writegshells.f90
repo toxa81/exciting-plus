71e1cc78c5c8801bdfd50a68e6aba32e6a4725e4
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-11-14 09:50:45 +0000

    fixed format of the output

--- trunk/src/addons/writegshells.f90
+++ trunk/src/addons/writegshells.f90
@@ -17,13 +17,13 @@ write(170,*)
 write(170,'("  G-vec.       lat.coord.      length(a.u.)   shell ")')
 write(170,'(" ---------------------------------------------------")')
 do ig=1,ishellng(ngsh,2)
-  write(170,'(2X,I4,4X,3I5,4X,F12.6,5x,I4)')ig,ivg(:,ig),gc(ig),igishell(ig)
+  write(170,'(2X,I8,4X,3I5,4X,F12.6,5x,I4)')ig,ivg(:,ig),gc(ig),igishell(ig)
 enddo
 write(170,*)
 write(170,'("  G-shell    num. G-vec in shell      total num. G-vec. ")')
 write(170,'(" -------------------------------------------------------")')
 do ish=1,ngsh
-  write(170,'(2X,I4,14X,I4,18X,I4)')ish,ishellng(ish,1),ishellng(ish,2)
+  write(170,'(2X,I4,14X,I4,18X,I8)')ish,ishellng(ish,1),ishellng(ish,2)
 enddo
 close(170)
 
