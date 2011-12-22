098cbc993418e980242c5c1c19b35240ead3a0cc
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2011-12-22 14:45:07 +0000

    work on cpp code

--- src/addons/cpp/constants.h
+++ src/addons/cpp/constants.h
@@ -2,7 +2,9 @@
 #define __CONSTANTS_H__
 
 const int blas_worker = 1;
-const int cublas_worker = 2;
+const int lapack_worker = 2;
+const int cublas_worker = 3;
+const int magma_worker = 4;
 
 #endif // __CONSTANTS_H__
 
