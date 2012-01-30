d777afb8442efbb1c049df295f4a1af5899a52b4
a.v.kozhevnikov@gmail.com <a.v.kozhevnikov@gmail.com@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2012-01-30 21:24:23 +0000

    some fixes with dependencies

--- trunk/src/addons/cpp/gpu_interface.h
+++ trunk/src/addons/cpp/gpu_interface.h
@@ -1,13 +1,6 @@
 #ifndef _GPU_INTERFACE_H_
 #define _GPU_INTERFACE_H_
 
-#include <assert.h>
-#include <cublas.h>
-#include <cublas_v2.h>
-#include <magmablas.h>
-#include <magma_lapack.h>
-#include <magma.h>
-
 extern "C" void gpu_malloc(void **ptr, int size);
 
 extern "C" void gpu_free(void *ptr);
