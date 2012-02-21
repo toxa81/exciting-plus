6d7261bd2bab5a714d7a1dbb60bfb6b354bc87f3
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2012-02-21 18:16:08 +0000

    spline integration in addons/sic/sic_main.f90

--- src/addons/sic/sic_genwanlm.f90
+++ src/addons/sic/sic_genwanlm.f90
@@ -8,17 +8,19 @@ complex(8), intent(inout) :: wantp(s_ntp,s_nr,nspinor)
 complex(8), intent(inout) :: wanlm(lmmaxwan,s_nr,nspinor)
 ! local variables
 integer ispn,lm,ir,l,m
-real(8) t1,t2
+real(8) t1
 complex(8), allocatable :: wantp1(:,:,:)
+real(8), allocatable :: f(:)
 !
 do ispn=1,nspinor
   call sic_zbsht(s_nr,wantp(1,1,ispn),wanlm(1,1,ispn))
 enddo
 allocate(wantp1(s_ntp,s_nr,nspinor))
+allocate(f(s_nr))
 ! convert to spherical coordinates
 do ispn=1,nspinor
   call zgemm('T','N',s_ntp,s_nr,lmmaxwan,zone,s_ylmf,lmmaxwan,wanlm(1,1,ispn),&
-    lmmaxwan,zzero,wantp1(1,1,ispn),s_ntp)
+    &lmmaxwan,zzero,wantp1(1,1,ispn),s_ntp)
 enddo
 if (wproc) then
   write(fout,*)
@@ -26,28 +28,28 @@ if (wproc) then
   write(fout,'(80("-"))')
   write(fout,*)
   write(fout,'("imaginary part of the original wf (total, average)      : ",2G18.10)')&
-    sum(abs(dimag(wantp))),sum(abs(dimag(wantp)))/s_nr/s_ntp/nspinor
+    &sum(abs(dimag(wantp))),sum(abs(dimag(wantp)))/s_nr/s_ntp/nspinor
   write(fout,'("imaginary part of the reconstructed wf (total, average) : ",2G18.10)')&
-    sum(abs(dimag(wantp1))),sum(abs(dimag(wantp1)))/s_nr/s_ntp/nspinor
+    &sum(abs(dimag(wantp1))),sum(abs(dimag(wantp1)))/s_nr/s_ntp/nspinor
   write(fout,'("difference of functions (total,average)                 : ",2G18.10)')&
-    sum(abs(wantp-wantp1)),sum(abs(wantp-wantp1))/s_nr/s_ntp/nspinor
+    &sum(abs(wantp-wantp1)),sum(abs(wantp-wantp1))/s_nr/s_ntp/nspinor
   write(fout,*)
-  t2=0.d0
   do ispn=1,nspinor
     do l=0,lmaxwan
       t1=0.d0
       do m=-l,l
         lm=idxlm(l,m)
         do ir=1,s_nr
-          t1=t1+(abs(wanlm(lm,ir,ispn))**2)*s_rw(ir)
+          f(ir)=(abs(wanlm(lm,ir,ispn))**2)
         enddo
-      enddo
+        t1=t1+rintegrate(s_nr,s_r,f)
+      enddo !m
       write(fout,'("   l,ispn : ",2I4,"    norm : ",G18.10)')l,ispn,t1
-      t2=t2+t1
-    enddo
-  enddo
+    enddo !l
+  enddo !ispn
   call flushifc(fout)
 endif
-deallocate(wantp1)
+deallocate(wantp1,f)
 return
 end
+
