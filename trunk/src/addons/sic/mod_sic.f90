d342baf1846b6b951b5af319e8ee497d4c9347da
a.v.kozhevnikov <a.v.kozhevnikov@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2012-02-21 17:50:59 +0000

    added spline integration

--- trunk/src/addons/sic/mod_sic.f90
+++ trunk/src/addons/sic/mod_sic.f90
@@ -694,4 +694,32 @@ endif
 return
 end subroutine
 
+!
+! integrate function usign spline fit
+!  
+real(8) function rintegrate(nr,r,f,m,g)
+implicit none
+integer, intent(in) :: nr
+real(8), intent(in) :: r(nr)
+real(8), intent(in) :: f(nr)
+integer, optional, intent(in) :: m
+real(8), optional, intent(out) :: g(nr)
+!
+real(8), allocatable :: f0(:),g0(:),cf(:,:)
+integer i,m0
+!
+! r^{m} weight
+m0=2
+if (present(m)) m0=m
+allocate(f0(nr),g0(nr),cf(4,nr))
+do i=1,nr
+  f0(i)=f(i)*(r(i)**m0)
+enddo
+call fderiv(-1,nr,r,f0,g0,cf)
+rintegrate=g(nr)
+if (present(g)) g(:)=g0(:)
+deallocate(f0,g0,cf)
+return
+end function
+
 end module
