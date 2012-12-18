98c051557450df638dcb82098ff90c7608c3fc44
a.v.kozhevnikov@gmail.com <a.v.kozhevnikov@gmail.com@b29bc608-f84f-0410-a42b-6dfa746fd12a>
2012-12-18 13:51:03 +0000

    don't write core eigen values

--- src/writeeval.f90
+++ src/writeeval.f90
@@ -35,21 +35,21 @@ do ik=1,nkpt
 end do
 close(50)
 ! write out the core eigenvalues
-open(50,file='EVALCORE'//trim(filext),action='WRITE',form='FORMATTED')
-do is=1,nspecies
-  do ia=1,natoms(is)
-    ias=idxas(ia,is)
-    write(50,*)
-    write(50,'("Species : ",I4," (",A,"), atom : ",I4)') is,trim(spsymb(is)),ia
-    do ist=1,spnst(is)
-      if (spcore(ist,is)) then
-        write(50,'(" n = ",I2,", l = ",I2,", k = ",I2," : ",G18.10)') &
-         spn(ist,is),spl(ist,is),spk(ist,is),evalcr(ist,ias)
-      end if
-    end do
-  end do
-end do
-close(50)
+!open(50,file='EVALCORE'//trim(filext),action='WRITE',form='FORMATTED')
+!do is=1,nspecies
+!  do ia=1,natoms(is)
+!    ias=idxas(ia,is)
+!    write(50,*)
+!    write(50,'("Species : ",I4," (",A,"), atom : ",I4)') is,trim(spsymb(is)),ia
+!    do ist=1,spnst(is)
+!      if (spcore(ist,is)) then
+!        write(50,'(" n = ",I2,", l = ",I2,", k = ",I2," : ",G18.10)') &
+!         spn(ist,is),spl(ist,is),spk(ist,is),evalcr(ist,ias)
+!      end if
+!    end do
+!  end do
+!end do
+!close(50)
 return
 end subroutine
 !EOC
